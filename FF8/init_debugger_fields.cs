0449547ae152f502aa52db57001d4fd6520503b0
Sebanisu <peeceseven@gmail.com>
2019-03-18 01:26:41 -0400

    put in varibles from memory into debugger fields
    used 3 varibles that weren't being used

--- FF8/init_debugger_fields.cs
+++ FF8/init_debugger_fields.cs
@@ -10,10 +10,10 @@ namespace FF8
             ArchiveWorker aw = new ArchiveWorker(Memory.Archives.A_FIELD);
             string[] lists = aw.GetListOfFiles();
             string maplist = lists.First(x => x.ToLower().Contains("mapdata.fs"));
-            maplist = maplist.Substring(0, maplist.Length - 2);
-            byte[] fs = ArchiveWorker.GetBinaryFile(Memory.Archives.A_FIELD, $"{maplist}fs");
-            byte[] fl = ArchiveWorker.GetBinaryFile(Memory.Archives.A_FIELD, $"{maplist}fl");
-            byte[] fi = ArchiveWorker.GetBinaryFile(Memory.Archives.A_FIELD, $"{maplist}fi");
+            maplist = maplist.Substring(0,maplist.Length - 3);
+            byte[] fs = ArchiveWorker.GetBinaryFile(Memory.Archives.A_FIELD, $"{maplist}{Memory.Archives.B_FileArchive}");
+            byte[] fl = ArchiveWorker.GetBinaryFile(Memory.Archives.A_FIELD, $"{maplist}{Memory.Archives.B_FileList}");
+            byte[] fi = ArchiveWorker.GetBinaryFile(Memory.Archives.A_FIELD, $"{maplist}{Memory.Archives.B_FileIndex}");
             string map = System.Text.Encoding.UTF8.GetString(fl).TrimEnd();
             string[] maplistb = System.Text.Encoding.UTF8.GetString(
                 ArchiveWorker.FileInTwoArchives(fi, fs, fl, map))
