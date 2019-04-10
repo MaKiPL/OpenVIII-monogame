d675d367e182ef3b5612044569183fca93eb2484
Sebanisu <peeceseven@gmail.com>
2019-04-10 14:13:44 -0400

    menubg is working
    Pretty good state with icon drawing, menu bg and such

--- FF8/Entry.cs
+++ FF8/Entry.cs
@@ -56,6 +56,17 @@ namespace FF8
         public Loc GetLoc => loc; public Rectangle GetRectangle => src;
 
         public sbyte CustomPallet { get; internal set; } = -1;
+        public Vector2 Tile { get; internal set; } = Vector2.Zero;
+        public bool Snap_Right { get; internal set; } = false;
+        /// <summary>
+        /// point where you want to stop drawing from right side so -8 would stop 8*scale pixels from edge
+        /// </summary>
+        public sbyte Offset_X2 { get; internal set; }
+        /// <summary>
+        /// point where you want to stop drawing from bottom side so -8 would stop 8*scale pixels from edge
+        /// </summary>
+        public sbyte Offset_Y2 { get; internal set; }
+        public bool Snap_Bottom { get; internal set; } = false;
 
         public byte LoadfromStreamSP2(BinaryReader br, UInt16 loc = 0, byte prevY = 0, byte fid = 0)
         {
@@ -110,17 +121,24 @@ namespace FF8
             rectangle = new Rectangle();
         }
 
+        public EntryGroup(params Entry[] entries)
+        {
+            list = new List<Entry>(entries.Length);
+            rectangle = new Rectangle();
+            Add(entries);
+        }
+
         #endregion Fields
 
         #region Properties
 
-        public int Height => rectangle.Height + Math.Abs(nag_offset.Y) + pos_offset.Y;
+        public int Height { get => rectangle.Height; private set => rectangle.Height = value; }
 
         public int Offset_X => 0;//nag_offset.X == 0 ? pos_offset.X : nag_offset.X;
 
         public int Offset_Y => 0;//nag_offset.Y == 0 ? pos_offset.Y : nag_offset.Y;
 
-        public int Width => rectangle.Width + Math.Abs(nag_offset.X) + pos_offset.X;
+        public int Width { get => rectangle.Width; private set => rectangle.Width = value; }
 
         public Rectangle GetRectangle => rectangle;
 
@@ -160,33 +178,22 @@ namespace FF8
                     }
                     //BarFix
                     //has 2 sides and no filling.
-                    if (list.Capacity == 2 && list.Count == 1 && 2 * Width + list[0].X == entry.X && list[0].Y == entry.Y)
-                        fillTexture = new Entry { X = (byte)(entry.X - Width), Y = entry.Y, Width = (ushort)Width, Height = (byte)Height, Offset_X = (short)Width };
+                    //if(entry.CurrentPos == 1872)
+                    if (list.Capacity == 2 && list.Count == 1 && (list[0].Width *2) + list[0].X == entry.X && list[0].Y == entry.Y)
+                        Add(new Entry {
+                            X = (byte)(entry.X - list[0].Width),
+                            Y = entry.Y, Width = list[0].Width,
+                            Height = list[0].Height,
+                            Tile=Vector2.UnitX,
+                            Offset_X = (short)list[0].Width,
+                            Offset_X2 = (sbyte)list[0].Width});
+                        
                 }
                 list.Add(entry);
-                //if (rectangle.X > entry.Offset_X)
-                //    rectangle.X = entry.Offset_X;
-                Rectangle src = entry.GetRectangle;
-                src.X += entry.Offset_X;
-                src.Y += entry.Offset_Y;
-                
-                bool contain = rectangle.Contains(src);
-                if (Width == 0 || /*Width > entry.Width &&*/ !contain)
-                    rectangle.Width = entry.Width;
-                if (entry.Offset_X < nag_offset.X)
-                    nag_offset.X = entry.Offset_X;
-                if (pos_offset.X == 0 && entry.Offset_X > pos_offset.X)
-                    pos_offset.X = entry.Offset_X;
-                //if (rectangle.Y > entry.Offset_Y)
-                //    rectangle.Y = entry.Offset_Y;
-                if (Height == 0 || /*Height < entry.Height && */ !contain)
-                    rectangle.Height = entry.Height;
-                if (entry.Offset_Y < nag_offset.Y)
-                    nag_offset.Y = entry.Offset_Y;
-                if (pos_offset.Y == 0 || entry.Offset_Y > pos_offset.Y)
-                    pos_offset.Y = entry.Offset_Y;
-
-                //rectangle.Height += entry.Height - (entry.Height + entry.Offset_Y);
+                ushort width = (ushort)(Math.Abs(entry.Offset_X) + entry.Width);
+                ushort height = (ushort)(Math.Abs(entry.Offset_Y) + entry.Height);
+                if (Width < width) Width = width;
+                if (Height < height) Height = height;
             }
         }
 
@@ -216,18 +223,30 @@ namespace FF8
                     //vscale = (float)dst.Height / Height;
                     vscale = hscale = (float)dst.Width / Width;
                 }
-                dst.X += (int)(e.Offset_X * hscale);
-                dst.Y += (int)(e.Offset_Y * vscale);
+                Point offset = new Point((int)(e.Offset_X * hscale), (int)(e.Offset_Y * vscale));
+                Point offset2 = new Point((int)(e.Offset_X2 * hscale), (int)(e.Offset_Y2 * vscale));
+                //if(e.Snap_Right==true)
+                //{
+                //}
+                dst.X += (e.Snap_Right ? inputdst.Width : 0) + offset.X;
+                dst.Y += (e.Snap_Bottom ? inputdst.Height : 0) + offset.Y;
                 dst.Width = (int)(e.Width * hscale);
                 dst.Height = (int)(e.Height * vscale);
                 do
                 {
-                    //dst.X += (int)(e.Width * scale);
-                    //dst.X += (int)(e.X * scale);
-                    Memory.spriteBatch.Draw(textures[cpallet], dst, e.GetRectangle, Color.White * fade);
-                    dst.Y += dst.Height;
+                    do
+                    {
+                        //if (e.Tile.Y > 0 && dst.Y > inputdst.Height)
+                        //    dst.Y -= offset.Y % inputdst.Height;
+                        //if (e.Tile.X > 0 && dst.X > inputdst.Width)
+                        //    dst.X -= offset.X % inputdst.Width;
+                        //dst.X += (int)(e.Width * scale);
+                        //dst.X += (int)(e.X * scale);
+                        Memory.spriteBatch.Draw(textures[cpallet], dst, e.GetRectangle, Color.White * fade);
+                    }
+                    while (e.Tile.Y > 0 && (dst.Y += dst.Height) < inputdst.Y + inputdst.Height-offset2.Y);
                 }
-                while (dst.Y < inputdst.Height);
+                while (e.Tile.X > 0 && (dst.X += dst.Width) < inputdst.X + inputdst.Width-offset2.X);
                 //Memory.spriteBatch.Draw(icons[pallet], Vector2.Zero, e.GetRectangle, Color.White*fade,0f,Vector2.Zero,Vector2.UnitX,SpriteEffects.None,0f);
             }
             Memory.SpriteBatchEnd();
--- FF8/Icons.cs
+++ FF8/Icons.cs
@@ -58,7 +58,7 @@ namespace FF8
                             //if (locs[i].count > 1) Count += (uint)(locs[i].count - 1);
                         }
 
-                        entries = new EntryGroup[Count+=2];
+                        entries = new EntryGroup[Count += 4];
 
                         for (int i = 0; i < locs.Length; i++)
                         {
@@ -68,6 +68,7 @@ namespace FF8
                             for (int e = 0; e < c; e++)
                             {
                                 Entry tmp = new Entry();
+                                //tmp.CurrentPos = (ushort)ms.Position;
                                 tmp.LoadfromStreamSP1(br);
                                 tmp.Part = (byte)e;
                                 tmp.SetLoc(locs[i]);
@@ -78,11 +79,12 @@ namespace FF8
                     //custom
                     Entry BG = new Entry
                     {
-                        X=0,
-                        Y=48,
-                        Width=256,
-                        Height=16,
-                        CustomPallet = 1
+                        X = 0,
+                        Y = 48,
+                        Width = 256,
+                        Height = 16,
+                        CustomPallet = 1,
+                        Tile = Vector2.UnitY
                     };
                     Entry BG2 = new Entry
                     {
@@ -91,20 +93,117 @@ namespace FF8
                         Offset_X = 256,
                         Width = 112,
                         Height = 16,
-                        CustomPallet = 1
+                        CustomPallet = 1,
+                        Tile = Vector2.UnitY
                     };
                     Entry Border_TopLeft = new Entry
                     {
                         X = 16,
                         Y = 0,
                         Width = 8,
-                        Height = 8
+                        Height = 8,
+                        CustomPallet = 0
                     };
-                    entries[Count - 1] = new EntryGroup(3);
-                    entries[Count - 1].Add(BG,BG2, Border_TopLeft);
-                    entries[Count - 2] = new EntryGroup(3);
-                    entries[Count - 2].Add(BG, Border_TopLeft);
+                    Entry Border_Top = new Entry
+                    {
+                        X = 24,
+                        Y = 0,
+                        Width = 8,
+                        Height = 8,
+                        Tile = Vector2.UnitX,
+                        CustomPallet = 0
+                    };
+                    Entry Border_Bottom = new Entry
+                    {
+                        X = 24,
+                        Y = 16,
+                        Width = 8,
+                        Height = 8,
+                        Tile = Vector2.UnitX,
+                        Snap_Bottom = true,
+                        Offset_Y = -8,
+                        CustomPallet = 0
+                    };
+                    Entry Border_TopRight = new Entry
+                    {
+                        X = 32,
+                        Y = 0,
+                        Width = 8,
+                        Height = 8,
+                        Snap_Right = true,
+                        Offset_X = -8,
+                        CustomPallet = 0
+                    };
+                    Entry Border_Left = new Entry
+                    {
+                        X = 16,
+                        Y = 8,
+                        Width = 8,
+                        Height = 8,
+                        Tile = Vector2.UnitY,
+                        CustomPallet = 0
+                    };
+                    Entry Border_Right = new Entry
+                    {
+                        X = 32,
+                        Y = 8,
+                        Width = 8,
+                        Height = 8,
+                        Tile = Vector2.UnitY,
+                        Snap_Right = true,
+                        Offset_X = -8,
+                        CustomPallet = 0
+                    };
+                    Entry Border_BottomLeft = new Entry
+                    {
+                        X = 16,
+                        Y = 16,
+                        Width = 8,
+                        Height = 8,
+                        Snap_Bottom = true,
+                        Offset_Y = -8,
+                        CustomPallet = 0
+                    };
+                    Entry Border_BottomRight = new Entry
+                    {
+                        X = 32,
+                        Y = 16,
+                        Width = 8,
+                        Height = 8,
+                        Snap_Bottom = true,
+                        Snap_Right = true,
+                        Offset_X = -8,
+                        Offset_Y = -8,
+                        CustomPallet = 0
+                    };
+                    Entry LoadBar_Empty = new Entry
+                    {
+                        X = 16,
+                        Y = 24,
+                        Width = 8,
+                        Height = 8,
+                        Tile=Vector2.UnitX,
+                        CustomPallet = 0
+                    };
+                    Entry LoadBar_Piece = new Entry
+                    {
+                        X = 0,
+                        Y = 16,
+                        Width = 8,
+                        Height = 8,
+                        Tile = Vector2.UnitX,
+                        Offset_Y = 1,
+                        CustomPallet = 5
+                    };
 
+                    //Border_TopRight.Offset_X = (short)(BG2.Offset_X + BG2.Width + -8);
+                    //Border_Right.Offset_X = (short)(BG2.Offset_X + BG2.Width + -8);
+                    entries[Count - 1] = new EntryGroup(BG, BG2, Border_Top, Border_Left, Border_Right, Border_Bottom, Border_TopLeft, Border_TopRight, Border_BottomLeft, Border_BottomRight);
+                    //Border_TopRight.Offset_X = (short)(BG2.Offset_X + -8);
+                    //Border_Right.Offset_X = (short)(BG2.Offset_X + BG2.Width + -8);
+                    entries[Count - 2] = new EntryGroup(BG, Border_Top, Border_Left, Border_Right, Border_Bottom, Border_TopLeft, Border_TopRight, Border_BottomLeft, Border_BottomRight);
+                    entries[Count - 3] = new EntryGroup(LoadBar_Piece);
+                    entries[Count - 4] = new EntryGroup(LoadBar_Empty);
                 }
             }
         }
@@ -129,21 +228,18 @@ namespace FF8
         #endregion Properties
 
         #region Methods
-        public EntryGroup GetEntryGroup(ID id) => GetEntryGroup((int)id);
-        public EntryGroup GetEntryGroup(int id) => entries[id];
-        public Entry GetEntry(ID id,int index = 0 ) => GetEntry((int)id);
 
-        public Entry GetEntry(int id,int index = 0 ) => entries[id][index] ?? null;
+        public EntryGroup GetEntryGroup(ID id) => GetEntryGroup((int)id);
+
+        public EntryGroup GetEntryGroup(int id) => entries[id];
+
+        public Entry GetEntry(ID id, int index = 0) => GetEntry((int)id);
+
+        public Entry GetEntry(int id, int index = 0) => entries[id][index] ?? null;
 
         internal void Draw(ID id, int pallet, Rectangle dst, float scale = 1f, float fade = 1f) => Draw((int)id, pallet, dst, scale, fade);
 
-        internal void Draw(int id, int pallet, Rectangle dst, float scale = 1f, float fade = 1f)
-        {
-            entries[id].Draw(icons,pallet, dst,scale, fade);
-        
-            //Memory.font.RenderBasicText(Font.CipherDirty($"pos: {entries[id].GetLoc().pos}\ncount: {entries[id].GetLoc().count}\n\nid: {id}\n\nUNKS: {string.Join(", ", entries[id].UNK)}\nALTS: {string.Join(", ", Array.ConvertAll(entries[id].UNK, item => (sbyte)item))}\n\npallet: {pallet}\nx: {entries[id].X}\ny: {entries[id].Y}\nwidth: {entries[id].Width}\nheight: {entries[id].Height} \n\nOffset X: {entries[id].Offset_X}\nOffset Y: {entries[id].Offset_Y}"), (int)(vp.Width * 0.10f), (int)(vp.Height * 0.05f), 1f, 2f, 0, 1);
-
-        }
+        internal void Draw(int id, int pallet, Rectangle dst, float scale = 1f, float fade = 1f) => entries[id].Draw(icons, pallet, dst, scale, fade);//Memory.font.RenderBasicText(Font.CipherDirty($"pos: {entries[id].GetLoc().pos}\ncount: {entries[id].GetLoc().count}\n\nid: {id}\n\nUNKS: {string.Join(", ", entries[id].UNK)}\nALTS: {string.Join(", ", Array.ConvertAll(entries[id].UNK, item => (sbyte)item))}\n\npallet: {pallet}\nx: {entries[id].X}\ny: {entries[id].Y}\nwidth: {entries[id].Width}\nheight: {entries[id].Height} \n\nOffset X: {entries[id].Offset_X}\nOffset Y: {entries[id].Offset_Y}"), (int)(vp.Width * 0.10f), (int)(vp.Height * 0.05f), 1f, 2f, 0, 1);
 
         internal void Draw(Rectangle dst, int pallet, float fade = 1f) => Memory.spriteBatch.Draw(icons[pallet], dst, Color.White * fade);
 
