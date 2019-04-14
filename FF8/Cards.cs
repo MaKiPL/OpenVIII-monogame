2324a55dd5377be108f3676a5c863f4edcf17694
Sebanisu <peeceseven@gmail.com>
2019-04-13 22:30:21 -0400

    It's loading the array data correctly but when I call the inharreted draw function it draws faces instead.

--- FF8/Cards.cs
+++ FF8/Cards.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,18 @@ using System.Threading.Tasks;
 
 namespace FF8
 {
-    class Cards : Faces, I_SP2
+    class Cards : Faces
     {
-        protected new const int TextureCount = 10;
-        protected new const string TextureFilename = "mc{0:00}.tex";
+        private const int TextureCount = 10;
+        private const string TextureFilename = "mc{0:00}.tex";
+        private const int TextureStartOffset = 0;
+        private const string IndexFilename = "cardanm.sp2";
 
-        public Cards() : base()
-        {
-        }
+
+        private static Texture2D[] textures;
+
+        private static Dictionary<Enum, Entry> entries;
+
+        public Cards() => Process(TextureCount, TextureFilename, TextureStartOffset, IndexFilename,ref entries,ref textures);
     }
 }
--- FF8/Faces.cs
+++ FF8/Faces.cs
@@ -10,62 +10,21 @@ namespace FF8
     internal partial class Faces : I_SP2
     {
         #region Fields
-        protected const int TextureCount=2;
-        protected const string TextureFilename = "face{0}.tex";
-        protected static readonly Texture2D[] textures = new Texture2D[TextureCount];
 
-        protected static Dictionary<ID, Entry> entries;
+        private const int TextureCount = 2;
+        private const string TextureFilename = "face{0:0}.tex";
+        private const int TextureStartOffset = 1;
+        private const string IndexFilename = "face.sp2";
+
+        private static Texture2D[] textures;
+
+        private static Dictionary<Enum, Entry> entries;
 
         #endregion Fields
 
         #region Constructors
 
-        public Faces()
-        {
-            if (entries == null)
-            {
-                ArchiveWorker aw = new ArchiveWorker(Memory.Archives.A_MENU);
-                byte[] test = ArchiveWorker.GetBinaryFile(Memory.Archives.A_MENU,
-                    aw.GetListOfFiles().First(x => x.ToLower().Contains("face.sp2")));
-                using (MemoryStream ms = new MemoryStream(test))
-                {
-                    ushort[] locs;
-                    using (BinaryReader br = new BinaryReader(ms))
-                    {
-                        ms.Seek(4, SeekOrigin.Begin);
-                        locs = new UInt16[Count];//br.ReadUInt32(); 32 valid values in face.sp2 rest is invalid
-                        entries = new Dictionary<ID, Entry>((int)Count);
-                        for (uint i = 0; i < Count; i++)
-                        {
-                            locs[i] = br.ReadUInt16();
-                            ms.Seek(2, SeekOrigin.Current);
-                        }
-                        byte fid = 0;
-                        Entry Last = null;
-                        for (uint i = 0; i < Count; i++)
-                        {
-                            entries[(ID)i] = new Entry();
-                            fid = entries[(ID)i].LoadfromStreamSP2(br, locs[i],(byte)(Last!=null?Last.Y:0), fid);
-
-                            Last = entries[(ID)i];
-
-
-                        }
-                    }
-                }
-                //using (FileStream fs = File.OpenWrite(Path.Combine("d:\\", "face.sp2")))
-                //{
-                //    fs.Write(test, 0, test.Length);
-                //}
-                TEX tex;
-                for (int i = 0; i < TextureCount; i++)
-                {
-                    tex = new TEX(ArchiveWorker.GetBinaryFile(Memory.Archives.A_MENU,
-                        aw.GetListOfFiles().First(x => x.ToLower().Contains(String.Format(TextureFilename,i)))));
-                    textures[0] = tex.GetTexture();
-                }
-            }
-        }
+        public Faces() => Process(TextureCount, TextureFilename, TextureStartOffset, IndexFilename, ref entries, ref textures);
 
         #endregion Constructors
 
@@ -113,7 +72,7 @@ namespace FF8
 
         #region Properties
 
-        public uint Count => 32;
+        public uint Count {get;set;}
 
         public uint PalletCount => 1;
 
@@ -121,14 +80,64 @@ namespace FF8
 
         #region Methods
 
-        public Entry GetEntry(Enum id) => entries[(ID)id];
+        protected void Process(int tc, string fn, int tso, string ind, ref Dictionary<Enum, Entry> dict, ref Texture2D[] texs)
+        {
+            if (dict == null)
+            {
+                texs = new Texture2D[tc];
+                ArchiveWorker aw = new ArchiveWorker(Memory.Archives.A_MENU);
+                byte[] test = ArchiveWorker.GetBinaryFile(Memory.Archives.A_MENU,
+                    aw.GetListOfFiles().First(x => x.ToLower().Contains(ind)));
+                using (MemoryStream ms = new MemoryStream(test))
+                {
+                    ushort[] locs;
+                    using (BinaryReader br = new BinaryReader(ms))
+                    {
+                        //ms.Seek(4, SeekOrigin.Begin);
+                        Count = br.ReadUInt32();
+                        locs = new ushort[Count];//br.ReadUInt32(); 32 valid values in face.sp2 rest is invalid
+                        dict = new Dictionary<Enum, Entry>((int)Count);
+                        for (uint i = 0; i < Count; i++)
+                        {
+                            locs[i] = br.ReadUInt16();
+                            ms.Seek(2, SeekOrigin.Current);
+                        }
+                        byte fid = 0;
+                        Entry Last = null;
+                        for (uint i = 0; i < Count; i++)
+                        {
+                            ms.Seek(locs[i] + 6, SeekOrigin.Begin);
+                            byte t = br.ReadByte();
+                            if (t == 0 || t == 96) continue;
+                            dict[(ID)i] = new Entry();
+                            fid = dict[(ID)i].LoadfromStreamSP2(br, locs[i], (byte)(Last != null ? Last.Y : 0), fid);
+
+                            Last = dict[(ID)i];
+                        }
+                    }
+                }
+                //using (FileStream fs = File.OpenWrite(Path.Combine("d:\\", "face.sp2")))
+                //{
+                //    fs.Write(test, 0, test.Length);
+                //}
+                TEX tex;
+                for (int i = 0; i < tc; i++)
+                {
+                    tex = new TEX(ArchiveWorker.GetBinaryFile(Memory.Archives.A_MENU,
+                        aw.GetListOfFiles().First(x => x.ToLower().Contains(string.Format(fn, i + tso)))));
+                    texs[i] = tex.GetTexture();
+                }
+            }
+        }
+
+        public Entry GetEntry(Enum id) => entries[id];
 
         public Entry GetEntry(int id) => entries[(ID)id];
 
-        public void Draw(Enum id, Rectangle dst, float fade = 1f) => Memory.spriteBatch.Draw(textures[entries[(ID)id].File], dst, entries[(ID)id].GetRectangle, Color.White * fade);
-        public void Draw(int id, Rectangle dst, float fade = 1f) => Draw((ID)id, dst, fade); 
+        public void Draw(Enum id, Rectangle dst, float fade = 1f) => Memory.spriteBatch.Draw(textures[entries[id].File], dst, entries[id].GetRectangle, Color.White * fade);
+
+        public void Draw(int id, Rectangle dst, float fade = 1f) => Draw((ID)id, dst, fade);
 
         #endregion Methods
-
     }
 }
