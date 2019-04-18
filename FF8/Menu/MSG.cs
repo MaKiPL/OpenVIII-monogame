c3e4219b6363ef6cee2a0d938c9ff95f6c3140fe
Sebanisu <peeceseven@gmail.com>
2019-04-18 04:40:59 -0400

    Started to add a MSG class to read some string arrays from msg files. noticed some characters were missing from our char table. So i used the texture as a guide and went down the list and added the special characters. I put all this in a excel spreadsheet so i could read the msg files and see what's in there.

---
+++ FF8/Menu/MSG.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF8
+{
+    /// <summary>
+    /// MSG files are FF8 Scrabled text strings. Each string ending with a \0
+    /// </summary>
+    class MSG
+    {
+        
+        //string[] DecodeDirty()
+        //{
+        //}
+    }
+}
