e63f414e9676a13f8ef3aec5c6d56be05926f8d1
Sebanisu <peeceseven@gmail.com>
2019-05-01 14:07:36 -0400

    more fully implemented save games. still much to do.

--- FF8/ff8files.cs
+++ FF8/ff8files.cs
@@ -9,12 +9,13 @@ namespace FF8
 
 
     internal struct FF8Variables
-    {    internal enum Characters
     {
-        // I noticed some values were in order of these characters so I made those values into arrays
-        // and put the character names into an enum.
-        Squall, Zell, Irvine, Quistis, Rinoa, Selphie, Seifer, Edea, Count // Count is the last value and is the number of characters in enum
-    }
+        internal enum Characters
+        {
+            // I noticed some values were in order of these characters so I made those values into arrays
+            // and put the character names into an enum.
+            Squall, Zell, Irvine, Quistis, Rinoa, Selphie, Seifer, Edea, Count // Count is the last value and is the number of characters in enum
+        }
         unsafe fixed byte byte03[4]; //[0-3]unused in fields (always "FF-8")
         private ulong Steps; //[4]Steps (used to generate random encounters)
 
@@ -343,8 +344,8 @@ namespace FF8
             public byte LimitBreakAngeloknown; //0x0B2B
             public byte[] LimitBreakAngelopoints; //0x0B2C
             public byte[] Itemsbattleorder; //0x0B34
-            public byte[] Items; //0x0B54 198 items (Item ID and Quantity)
-            public uint Gametime; //0x0CE0
+            public Item[] Items; //0x0B54 198 items (Item ID and Quantity)
+            public TimeSpan Gametime; //0x0CE0
             public uint Countdown; //0x0CE4
             public uint Unknown3; //0x0CE8
             public uint Battlevictorycount; //0x0CEC
@@ -352,7 +353,7 @@ namespace FF8
             public ushort Battlebattleescaped; //0x0CF2
             public uint Unknown5; //0x0CF4
             public uint BattleTonberrykilledcount; //0x0CF8
-            public uint BattleTonberrySrkilled; //0x0CFC (yeah, this is a boolean)
+            public bool BattleTonberrySrkilled; //0x0CFC (yeah, this is a boolean)
             public uint Unknown6; //0x0D00
             public uint BattleR1; //0x0D04 First "Bug" battle (R1 tip)
             public uint BattleELEMENTAL; //0x0D08 First "Bomb" battle (Elemental tip)
@@ -371,9 +372,9 @@ namespace FF8
             public ushort Module; //0x0D50 (1= field, 2= worldmap, 3= battle)
             public ushort Currentfield; //0x0D52
             public ushort Previousfield; //0x0D54
-            public byte[] CoordX; //0x0D56 signed  (party1, party2, party3)
-            public byte[] CoordY; //0x0D5C signed  (party1, party2, party3)
-            public byte[] Triangle_ID; //0x0D62  (party1, party2, party3)
+            public short[] CoordX; //0x0D56 signed  (party1, party2, party3)
+            public short[] CoordY; //0x0D5C signed  (party1, party2, party3)
+            public ushort[] Triangle_ID; //0x0D62  (party1, party2, party3)
             public byte[] Direction; //0x0D68  (party1, party2, party3)
             public byte Padding; //0x0D6B
             public uint Unknown8; //0x0D6C
@@ -407,6 +408,7 @@ namespace FF8
                 Grieversname = null;
             }
 
+            public struct Item { public byte ID; public byte QTY; };
             public void Read(BinaryReader br)
             {
                 LocationID = br.ReadUInt16();//0x0004
@@ -434,6 +436,66 @@ namespace FF8
                 Party = br.ReadBytes(4); //0x0B04 // 4 bytes 0xFF terminated.
                 KnownWeapons = br.ReadBytes(4); //0x0B08 // 4 bytes
                 Grieversname = br.ReadBytes(12); //0x0B0C // 12 bytes
+
+                Unknown = br.ReadUInt16();//0x0B18  (always 7966?)
+                Unknown = br.ReadUInt16();//0x0B1A 
+                AmountofGil2 = br.ReadUInt32();//0x0B1C //dupilicate
+                AmountofGil_Laguna = br.ReadUInt32();//0x0B20 
+                LimitBreakQuistis = br.ReadUInt16();//0x0B24 
+                LimitBreakZell = br.ReadUInt16();//0x0B26 
+                LimitBreakIrvine = br.ReadByte();//0x0B28 
+                LimitBreakSelphie = br.ReadByte();//0x0B29 
+                LimitBreakAngelocompleted = br.ReadByte();//0x0B2A 
+                LimitBreakAngeloknown = br.ReadByte();//0x0B2B 
+                LimitBreakAngelopoints = br.ReadBytes(8);//0x0B2C 
+                Itemsbattleorder = br.ReadBytes(32);//0x0B34
+                Items = new Item[198];
+                for (int i = 0; i < 198; i++)
+                    Items[0] = new Item { ID = br.ReadByte(), QTY = br.ReadByte() }; //0x0B54 198 items (Item ID and Quantity)
+                Gametime = new TimeSpan(0, 0, (int)br.ReadUInt32());//0x0CE0 
+                Countdown = br.ReadUInt32();//0x0CE4 
+                Unknown = br.ReadUInt32();//0x0CE8 
+                Battlevictorycount = br.ReadUInt32();//0x0CEC 
+                Unknown = br.ReadUInt16();//0x0CF0 
+                Battlebattleescaped = br.ReadUInt16();//0x0CF2 
+                Unknown = br.ReadUInt32();//0x0CF4 
+                BattleTonberrykilledcount = br.ReadUInt32();//0x0CF8 
+                BattleTonberrySrkilled = br.ReadUInt32()>0;//0x0CFC (yeah, this is a boolean)
+                Unknown = br.ReadUInt32();//0x0D00 
+                BattleR1 = br.ReadUInt32();//0x0D04 First "Bug" battle (R1 tip)
+                BattleELEMENTAL = br.ReadUInt32();//0x0D08 First "Bomb" battle (Elemental tip)
+                BattleMENTAL = br.ReadUInt32();//0x0D0C  First "T-Rex" battle (Mental tip)
+                BattleIRVINE = br.ReadUInt32();//0x0D10 First "Irvine" battle (Irvine's limit break tip)
+                BattleMAGIC = br.ReadBytes(8);//0x0D14 Magic drawn once
+                BattleSCAN = br.ReadBytes(20);//0x0D1C Ennemy scanned once
+                BattleRAUTO = br.ReadByte();//0x0D30 Renzokuken auto 
+                BattleRINDICATOR = br.ReadByte();//0x0D31 Renzokuken indicator
+                BattleUNK = br.ReadByte();//0x0D32 dream/Odin/Phoenix/Gilgamesh/Angelo disabled/Angel Wing enabled/???/???
+                Tutorialinfos = br.ReadBytes(16);//0x0D33 
+                SeeDtestlevel = br.ReadByte();//0x0D43 
+                Unknown = br.ReadUInt32();//0x0D44 
+                Party2 = br.ReadUInt32();//0x0D48 (last byte always = 255) //dupicate
+                Unknown = br.ReadUInt32();//0x0D4C 
+                Module = br.ReadUInt16();//0x0D50 (1= field, 2= worldmap, 3= battle)
+                Currentfield = br.ReadUInt16();//0x0D52 
+                Previousfield = br.ReadUInt16();//0x0D54 
+                CoordX = new short[3];
+                for (int i =0; i<3;i++)
+                    CoordX[i] = br.ReadInt16();//0x0D56 signed  (party1, party2, party3)
+                CoordY = new short [3];
+                for (int i = 0; i < 3; i++)
+                    CoordY[i] = br.ReadInt16();//0x0D5C signed  (party1, party2, party3)
+                Triangle_ID = new ushort[3];
+                for (int i = 0; i < 3; i++)
+                    Triangle_ID[i] = br.ReadUInt16();//0x0D62  (party1, party2, party3)
+                Direction = br.ReadBytes(3 * 1);//0x0D68  (party1, party2, party3)
+                Padding = br.ReadByte();//0x0D6B 
+                Unknown = br.ReadUInt32();//0x0D6C 
+                //Fieldvars = br.ReadBytes(256 + 1024);//0x0D70 http://wiki.ffrtt.ru/index.php/FF8/Variables
+                //Worldmap = br.ReadBytes(128);//0x1270 
+                //TripleTriad = br.ReadBytes(128);//0x12F0 
+                //ChocoboWorld = br.ReadBytes(64);//0x1370 
+
             }
         }
 
