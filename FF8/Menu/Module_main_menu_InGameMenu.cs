729bd1bd855ceae80f89ebb111eadf01eec0caee
Sebanisu <peeceseven@gmail.com>
2019-05-06 04:09:24 -0400

    Split some more objects into their own files. added overloads to item.

--- FF8/Menu/Module_main_menu_InGameMenu.cs
+++ FF8/Menu/Module_main_menu_InGameMenu.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FF8.Menu;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
@@ -8,8 +9,40 @@ namespace FF8
 {
     internal static partial class Module_main_menu_debug
     {
+        private static Dictionary<Enum, Item> strSideBar;
+        private static Dictionary<Enum, Item> strHeaderText;
+
         private static void Init_InGameMenu()
-        { }
+        {
+            strSideBar = new Dictionary<Enum, Item>()
+            {
+                { IGMItems.Junction, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,0) } },
+                { IGMItems.Item, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,2) } },
+                { IGMItems.Magic, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,4) } },
+                { IGMItems.Status, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,8) } },
+                { IGMItems.GF, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,6) } },
+                { IGMItems.Ability, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,62) } },
+                { IGMItems.Switch, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,64) } },
+                { IGMItems.Card, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,10) } },
+                { IGMItems.Config, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,16) } },
+                { IGMItems.Tutorial, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,67) } },
+                { IGMItems.Save, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,14) } },
+            };
+            strHeaderText = new Dictionary<Enum, Item>()
+            {
+                { IGMItems.Junction, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,1) } },
+                { IGMItems.Item, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,3) } },
+                { IGMItems.Magic, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,5) } },
+                { IGMItems.Status, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,9) } },
+                { IGMItems.GF, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,7) } },
+                { IGMItems.Ability, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,63) } },
+                { IGMItems.Switch, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,65) } },
+                { IGMItems.Card, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,11) } },
+                { IGMItems.Config, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,17) } },
+                { IGMItems.Tutorial, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,68) } },
+                { IGMItems.Save, new Item{Text=Memory.Strings.Read(Strings.FileID.MNGRP, 0 ,15) } },
+            };
+        }
 
 
         private static void UpdateInGameMenu() => throw new NotImplementedException();
@@ -57,5 +90,23 @@ namespace FF8
 
         }
 
+    }
+}
+
+namespace FF8.Menu
+{
+    public enum IGMItems
+    {
+        Junction,
+        Item,
+        Magic,
+        Status,
+        GF,
+        Ability,
+        Switch,
+        Card,
+        Config,
+        Tutorial,
+        Save
     }
 }
--- FF8/Menu/Strings.cs
+++ FF8/Menu/Strings.cs
@@ -6,34 +6,6 @@ using System.Text;
 
 namespace FF8
 {
-    public class Stringfile
-    {
-        #region Fields
-
-        public Dictionary<uint, List<uint>> sPositions;
-        public List<Loc> subPositions;
-
-        #endregion Fields
-
-        #region Constructors
-
-        public Stringfile(Dictionary<uint, List<uint>> sPositions, List<Loc> subPositions)
-        {
-            this.sPositions = sPositions;
-            this.subPositions = subPositions;
-        }
-
-        /// <summary>
-        /// do not use this.
-        /// </summary>
-        private Stringfile()
-        {
-            this.sPositions = null;
-            this.subPositions = null;
-        }
-
-        #endregion Constructors
-    }
 
     /// <summary>
     /// Loads strings from FF8 files
