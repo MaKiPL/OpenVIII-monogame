c2a69dee8adb81c9ef2be26d599861d54e771ffa
Sebanisu <peeceseven@gmail.com>
2019-05-09 18:38:29 -0400

    Update Kenerl_bin.cs
    all the stats have formulas so i'm hiding the stats and going to add functions you can feed values into and get the stats.

--- FF8/Menu/Kenerl_bin.cs
+++ FF8/Menu/Kenerl_bin.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using FF8.Menu;
+using System.Collections.Generic;
 using System.IO;
 
 namespace FF8
@@ -15,35 +16,36 @@ namespace FF8
                                    //Squall and Rinoa have name offsets of 0xFFFF because their name is in the save game data rather than kernel.bin.
 
             public byte Crisis; //0x0002; 1 byte; Crisis level hp multiplier
-            public byte Gender; //0x0003; 1 byte; Gender; 0x00 - Male 0x01 - Female
+            public Gender Gender; //0x0003; 1 byte; Gender; 0x00 - Male 0x01 - Female
             public byte LimitID; //0x0004; 1 byte; Limit Break ID
             public byte LimitParam; //0x0005; 1 byte; Limit Break Param used for the power of each renzokuken hit before finisher
-            public ushort EXP; //0x0006; 2 bytes; EXP modifier
-            public uint HP; //0x0008; 4 bytes; HP
-            public uint STR; //0x000C; 4 bytes; STR
-            public uint VIT; //0x0010; 4 bytes; VIT
-            public uint MAG; //0x0014; 4 bytes; MAG
-            public uint SPR; //0x0018; 4 bytes; SPR
-            public uint SPD; //0x001C; 4 bytes; SPD
-            public uint LUCK; //0x0020; 4 bytes; LUCK
+            private byte[] _EXP; //0x0006; 2 bytes; EXP modifier
+            private byte[] _HP; //0x0008; 4 bytes; HP
+            private byte[] _STR; //0x000C; 4 bytes; STR
+            private byte[] _VIT; //0x0010; 4 bytes; VIT
+            private byte[] _MAG; //0x0014; 4 bytes; MAG
+            private byte[] _SPR; //0x0018; 4 bytes; SPR
+            private byte[] _SPD; //0x001C; 4 bytes; SPD
+            private byte[] _LUCK; //0x0020; 4 bytes; LUCK
 
             public void Read(BinaryReader br)
             {
                 Offset = br.ReadUInt16(); //0x0000; 2 bytes; Offset to character name
                                           //Squall and Rinoa have name offsets of 0xFFFF because their name is in the save game data rather than kernel.bin.
                 Crisis = br.ReadByte(); //0x0002; 1 byte; Crisis level hp multiplier
-                Gender = br.ReadByte(); //0x0003; 1 byte; Gender; 0x00 - Male 0x01 - Female
+                Gender = br.ReadByte()==0?Gender.Male:Gender.Female; //0x0003; 1 byte; Gender; 0x00 - Male 0x01 - Female
                 LimitID = br.ReadByte(); //0x0004; 1 byte; Limit Break ID
                 LimitParam = br.ReadByte(); //0x0005; 1 byte; Limit Break Param used for the power of each renzokuken hit before finisher
-                EXP = br.ReadUInt16(); //0x0006; 2 bytes; EXP modifier
-                HP = br.ReadUInt32(); //0x0008; 4 bytes; HP
-                STR = br.ReadUInt32(); //0x000C; 4 bytes; STR
-                VIT = br.ReadUInt32(); //0x0010; 4 bytes; VIT
-                MAG = br.ReadUInt32(); //0x0014; 4 bytes; MAG
-                SPR = br.ReadUInt32(); //0x0018; 4 bytes; SPR
-                SPD = br.ReadUInt32(); //0x001C; 4 bytes; SPD
-                LUCK = br.ReadUInt32(); //0x0020; 4 bytes; LUCK
+                _EXP = br.ReadBytes(2); //0x0006; 2 bytes; EXP modifier
+                _HP = br.ReadBytes(4); //0x0008; 4 bytes; HP
+                _STR = br.ReadBytes(4); //0x000C; 4 bytes; STR
+                _VIT = br.ReadBytes(4); //0x0010; 4 bytes; VIT
+                _MAG = br.ReadBytes(4); //0x0014; 4 bytes; MAG
+                _SPR = br.ReadBytes(4); //0x0018; 4 bytes; SPR
+                _SPD = br.ReadBytes(4); //0x001C; 4 bytes; SPD
+                _LUCK = br.ReadBytes(4); //0x0020; 4 bytes; LUCK
             }
+            //public uint HP()
         }
 
         private ArchiveWorker aw;
@@ -67,5 +69,14 @@ namespace FF8
                 }
             }
         }
+    }
+}
+
+namespace FF8.Menu
+{
+    public enum Gender
+    {
+        Male,
+        Female
     }
 }
