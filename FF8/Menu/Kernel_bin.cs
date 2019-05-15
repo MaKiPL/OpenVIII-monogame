6ea4b637398efc6f6a802797d3769c885bc75495
Sebanisu <peeceseven@gmail.com>
2019-05-15 18:10:11 -0400

    filling out the classes wip

--- FF8/Menu/Kernel_bin.cs
+++ FF8/Menu/Kernel_bin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
@@ -168,6 +169,10 @@ namespace FF8
                 //br.BaseStream.Seek(4,SeekOrigin.Current);
             }
         }
+        /// <summary>
+        /// Command Ability
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/Command-ability-data"/>
         internal enum Command_ability
         {
             Recover,
@@ -228,59 +233,195 @@ namespace FF8
             }
         }
 
-
+        /// <summary>
+        /// Junction Abilities Data
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/Junction-abilities"/>
         internal class Junction_abilities
         {
             internal const int count = 20;
             internal const int id = 11;
+            public override string ToString() => Name;
+            internal FF8String Name { get; private set; }
+            internal FF8String Description { get; private set; }
+            public byte AP { get; private set; }
+            public BitArray J_Flags { get; private set; }
 
             internal void Read(BinaryReader br, int i)
             {
+
+                Name = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2);
+                //0x0000	2 bytes Offset to name
+                Description = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2 + 1);
+                //0x0002	2 bytes Offset to description
+                br.BaseStream.Seek(4,SeekOrigin.Current);
+                AP = br.ReadByte();
+                //0x0004  1 byte AP Required to learn ability
+                J_Flags = new BitArray(br.ReadBytes(3));
+                //0x0005  3 byte J-Flag
             }
         }
-
+        /// <summary>
+        /// Command Abilities
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/Command-abilities"/>
         internal class Command_abilities
         {
-            internal const int count = 12;
+            internal const int count = 19;
             internal const int id = 12;
+            public override string ToString() => Name;
+            internal FF8String Name { get; private set; }
+            internal FF8String Description { get; private set; }
+            public byte AP { get; private set; }
+            public byte Index { get; private set; }
+            public byte[] Unknown0 { get; private set; }
+
             internal void Read(BinaryReader br, int i)
             {
+                Name = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2);
+                //0x0000	2 bytes Offset to name
+                Description = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2 + 1);
+                //0x0002	2 bytes Offset to description
+
+                AP = br.ReadByte();
+                //0x0004  1 byte AP Required to learn ability
+                Index = br.ReadByte();
+                //0x0005  1 byte Index to Battle commands
+                Unknown0 = br.ReadBytes(2);
+                //0x0006  2 bytes Unknown/ Unused
             }
         }
-
+        /// <summary>
+        /// Stat Percentage Increasing Abilities Data
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/Stat-percentage-increasing-abilities"/>
         internal class Stat_percent_abilities
         {
-            internal const int count = 12;
+            internal const int count = 19;
             internal const int id = 13;
+
+            public FF8String Name { get; private set; }
+            public FF8String Description { get; private set; }
+            public byte AP { get; private set; }
+            public Stat Stat { get; private set; }
+            public byte Value { get; private set; }
+            public byte Unknown0 { get; private set; }
+
             internal void Read(BinaryReader br, int i)
             {
+                Name = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2);
+                //0x0000	2 bytes Offset to name
+                Description = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2 + 1);
+                //0x0002	2 bytes Offset to description
+                AP = br.ReadByte();
+                //0x0004  1 byte AP needed to learn the ability
+                Stat =(Stat) br.ReadByte();
+                //0x0005  1 byte Stat to increase
+                Value= br.ReadByte();
+                //0x0006  1 byte Increase value
+                Unknown0 = br.ReadByte();
+                //0x0007  1 byte Unknown/ Unused
             }
         }
-
+        /// <summary>
+        /// Characters Abilities Data
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/Character-abilities"/>
         internal class Character_abilities
         {
-            internal const int count = 12;
+            internal const int count = 20;
             internal const int id = 14;
+
+            public FF8String Name { get; private set; }
+            public FF8String Description { get; private set; }
+            public byte AP { get; private set; }
+            public BitArray Flags { get; private set; }
+
             internal void Read(BinaryReader br, int i)
             {
+                Name = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2);
+                //0x0000	2 bytes Offset to name
+                Description = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2 + 1);
+                //0x0002	2 bytes Offset to description
+                br.BaseStream.Seek(4, SeekOrigin.Current);
+                AP = br.ReadByte();
+                //0x0004  1 byte AP Required to learn ability
+                Flags = new BitArray(br.ReadBytes(3));
+                //0x0005  3 byte Flags
             }
         }
-
+        /// <summary>
+        /// Party Abilities Data
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/Party-abilities"/>
         internal class Party_abilities
         {
-            internal const int count = 12;
+            internal const int count = 5;
             internal const int id = 15;
+
+            public FF8String Name { get; private set; }
+            public FF8String Description { get; private set; }
+            public byte AP { get; private set; }
+            public BitArray Flags { get; private set; }
+            public byte[] Unknown0 { get; private set; }
+
             internal void Read(BinaryReader br, int i)
             {
+                Name = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2);
+                //0x0000	2 bytes Offset to name
+                Description = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2 + 1);
+                //0x0002	2 bytes Offset to description
+                br.BaseStream.Seek(4, SeekOrigin.Current);
+                AP = br.ReadByte();
+                //0x0004  1 byte AP Required to learn ability
+                Flags = new BitArray(br.ReadBytes(1));
+                //0x0005  1 byte Flags
+                Unknown0 = br.ReadBytes(2);
+                //0x0006  2 byte Unknown/ Unused
             }
         }
-
+        internal enum Stat
+        {
+            HP,
+            STR,
+            VIT,
+            MAG,
+            SPR,
+            SPD,
+            EVA,
+            HIT,
+            LUCK
+        }
+        /// <summary>
+        /// GF Abilities Data
+        /// </summary>
+        /// <see cref="https://github.com/alexfilth/doomtrain/wiki/GF-abilities"/>
         internal class GF_abilities
         {
-            internal const int count = 12;
+            internal const int count = 9;
             internal const int id = 16;
+
+            public FF8String Name { get; private set; }
+            public FF8String Description { get; private set; }
+            public byte AP { get; private set; }
+            public byte Boost { get; private set; }
+            public Stat Stat { get; private set; }
+            public byte Value { get; private set; }
+
             internal void Read(BinaryReader br, int i)
             {
+                Name = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2);
+                //0x0000	2 bytes Offset to name
+                Description = Memory.Strings.Read(Strings.FileID.KERNEL, id, i * 2 + 1);
+                //0x0002	2 bytes Offset to description
+                AP = br.ReadByte();
+                //0x0004 1 byte AP needed to learn the ability
+                Boost = br.ReadByte();
+                //0x0005 Enable Boost
+                Stat = (Stat)br.ReadByte();
+                //0x0006  1 byte Stat to increase
+                Value = br.ReadByte();
+                //0x0007  1 byte Increase value
             }
         }
 
