3b6497ae5451a21ba45269433d84365a9915cbf9
Sebanisu <peeceseven@gmail.com>
2019-05-29 14:11:18 -0400

    outfile is no longer static

--- FF8/LZSS.cs
+++ FF8/LZSS.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FF8
 {
-    class LZSS
+    internal class LZSS
     {
         //NEW LZSS
 
         #region License
+
         /**************************************************************
 LZSS.C -- A Data Compression Program
 (tab = 4 spaces)
@@ -23,62 +21,69 @@ PC-VAN		SCIENCE
 NIFTY-Serve	PAF01022
 CompuServe	74050,1022
 **************************************************************/
-        #endregion
 
-        
+        #endregion License
 
         private static readonly int N = 4096;
         private static readonly int F = 18;
         private static readonly int THRESHOLD = 2;
         private static readonly int EOF = -1;
 
-        static List<byte> outfile;
         public static byte[] DecompressAllNew(byte[] data)
         {
+            byte[] outfilearray;
             using (MemoryStream infile = new MemoryStream(data))
             {
-                outfile = new List<byte>();
-                Decode(infile);
+                Decode(infile, out outfilearray);
             }
-            return outfile.ToArray();
+            return outfilearray;
         }
 
         //Code borrowed from Java's implementation of LZSS by antiquechrono
-        private static void Decode(MemoryStream infile) 
+        private static void Decode(MemoryStream infile, out byte[] outfilearray)
         {
-        int i, j, k, r, c;
+            int i, j, k, r, c;
 
+            List<byte> outfile = new List<byte>();
 
-        int[] text_buf = new int[N + F - 1];    /* ring buffer of size N, with extra F-1 bytes to facilitate string comparison */
-        //unsigned int  flags;
-        int flags;
+            int[] text_buf = new int[N + F - 1];    /* ring buffer of size N, with extra F-1 bytes to facilitate string comparison */
+                                                    //unsigned int  flags;
+            int flags;
 
-        for (i = 0; i<N - F; i++) text_buf[i] = 0;
-        r = N - F;  flags = 0;
-        for ( ; ; ) {
-            if (((flags >>= 1) & 256) == 0) {
-                if ((c = infile.ReadByte()) == EOF) break;
-                flags = c | 0xff00;		/* uses higher byte cleverly */
-            }							/* to count eight */
-//            if (flags & 1) {
-            if ((flags & 1) == 1) {
-                if ((c = infile.ReadByte()) == EOF) break;
+            for (i = 0; i < N - F; i++) text_buf[i] = 0;
+            r = N - F; flags = 0;
+            for (; ; )
+            {
+                if (((flags >>= 1) & 256) == 0)
+                {
+                    if ((c = infile.ReadByte()) == EOF) break;
+                    flags = c | 0xff00;     /* uses higher byte cleverly */
+                }                           /* to count eight */
+                                            // if (flags & 1) {
+                if ((flags & 1) == 1)
+                {
+                    if ((c = infile.ReadByte()) == EOF) break;
                     outfile.Add((byte)c);
-                text_buf[r++] = c;
-                r &= (N - 1);
-            } else {
-                if ((i = infile.ReadByte()) == EOF) break;
-                if ((j = infile.ReadByte()) == EOF) break;
-                i |= ((j & 0xf0) << 4);  j = (j & 0x0f) + THRESHOLD;
-                for (k = 0; k <= j; k++) {
-                    c = text_buf[(i + k) & (N - 1)];
-                        outfile.Add((byte)c);
-                        text_buf[r++] = c;
+                    text_buf[r++] = c;
                     r &= (N - 1);
                 }
+                else
+                {
+                    if ((i = infile.ReadByte()) == EOF) break;
+                    if ((j = infile.ReadByte()) == EOF) break;
+                    i |= ((j & 0xf0) << 4); j = (j & 0x0f) + THRESHOLD;
+                    for (k = 0; k <= j; k++)
+                    {
+                        c = text_buf[(i + k) & (N - 1)];
+                        outfile.Add((byte)c);
+                        text_buf[r++] = c;
+                        r &= (N - 1);
+                    }
+                }
             }
+            outfilearray = outfile.ToArray();
         }
-    }
+
         /// <summary>
         /// Decompiles LZSS. You have to know the OutputSize. [DEPRECATED]
         /// </summary>
@@ -98,15 +103,14 @@ CompuServe	74050,1022
                     bDynamic = true;
                 }
 
-                var result = new byte[size];
+                byte[] result = new byte[size];
 
                 int curResult = 0;
                 int curBuff = 4078, flagByte = 0;
                 int fileData = 4,
                     endFileData = (int)fileSize;
 
-
-                var textBuf = new byte[4113];
+                byte[] textBuf = new byte[4113];
 
                 while (true)
                 {
@@ -157,6 +161,5 @@ CompuServe	74050,1022
             Array.Copy(result, buffer, buffer.Length);
             return buffer;
         }
-
     }
 }
