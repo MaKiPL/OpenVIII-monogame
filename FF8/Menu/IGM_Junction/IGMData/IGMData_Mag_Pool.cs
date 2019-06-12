7f4addd7c4ab873a2404e4efe810502e3ae6ba8a
Sebanisu <peeceseven@gmail.com>
2019-06-12 19:29:34 -0400

    Fixed page turn not replacing backup values before reloading list.

--- FF8/Menu/IGM_Junction/IGMData/IGMData_Mag_Pool.cs
+++ FF8/Menu/IGM_Junction/IGMData/IGMData_Mag_Pool.cs
@@ -7,14 +7,17 @@ namespace FF8
 {
     public partial class Module_main_menu_debug
     {
+
         #region Classes
 
         private partial class IGM_Junction
         {
+
             #region Classes
 
             private class IGMData_Mag_Pool : IGMData_Pool<Saves.CharacterData, byte>
             {
+
                 #region Fields
 
                 public static EventHandler<Mode> SlotConfirmListener;
@@ -40,13 +43,6 @@ namespace FF8
                 public int LastPage { get; private set; }
                 public Kernel_bin.Stat LastStat { get; private set; }
                 public IEnumerable<Kernel_bin.Magic_Data> Sort { get; private set; }
-                //public IGMData_Mag_Stat_Slots Stat_Slots { get; private set; }
-                //public IGMData_Mag_EL_A_D_Slots EL_A_D_Slots { get; private set; }
-                //public IGMData_Mag_ST_A_D_Slots ST_A_D_Slots { get; private set; }
-                //public IGMData_Mag_EL_A_Values EL_A_Values { get; private set; }
-                //public IGMData_Mag_EL_D_Values EL_D_Values { get; private set; }
-                //public IGMData_Mag_ST_A_Values ST_A_Values { get; private set; }
-                //public IGMData_Mag_ST_D_Values ST_D_Values { get; private set; }
                 public Mode SortMode { get; private set; }
 
                 public Kernel_bin.Stat Stat { get; private set; }
@@ -118,56 +114,24 @@ namespace FF8
                     }
                 }
 
-                public void Generate_Preview()
+                public void Generate_Preview(bool skipundo = false)
                 {
                     if (Stat != Kernel_bin.Stat.None && CURSOR_SELECT < Contents.Length)
                     {
                         Cursor_Status |= Cursor_Status.Enabled;
                         if (Source.Stat_J[Stat] != Contents[CURSOR_SELECT])
                         {
-                            SlotUndoListener?.Invoke(this, InGameMenu_Junction.GetMode());
-
-                            if (Memory.State.Characters != null)
-                                Source = Memory.State.Characters[Character];
+                            if (!skipundo)
+                            {
+                                Undo();
+                                skipundo = false;
+                            }
                             Source.JunctionSpell(Stat, Contents[CURSOR_SELECT]);
-
                             SlotReinitListener?.Invoke(this, InGameMenu_Junction.GetMode());
                         }
                     }
                 }
-
-                public void Get_Current_Slot_Value()
-                {
-                    //switch (SortMode)
-                    //{
-                    //    case Mode.Mag_Pool_Stat:
-                    //        Slots = Stat_Slots;
-                    //        Values = null;
-                    //        break;
-
-                    // case Mode.Mag_Pool_EL_A: Slots = EL_A_D_Slots; Values = EL_A_Values; break;
-
-                    // case Mode.Mag_Pool_EL_D: Slots = EL_A_D_Slots; Values = EL_D_Values; break;
-
-                    // case Mode.Mag_Pool_ST_A: Slots = ST_A_D_Slots; Values = ST_A_Values; break;
-
-                    // case Mode.Mag_Pool_ST_D: Slots = ST_A_D_Slots; Values = ST_D_Values; break;
-
-                    //    default:
-                    //        Slots = null;
-                    //        Values = null;
-                    //        break;
-                    //}
-                }
-
                 public void Get_Slots_Values() =>
-                    //Stat_Slots = (IGMData_Mag_Stat_Slots)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[0, 0]).Data;
-                    //EL_A_D_Slots = (IGMData_Mag_EL_A_D_Slots)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[3, 0]).Data;
-                    //ST_A_D_Slots = (IGMData_Mag_ST_A_D_Slots)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[6, 0]).Data;
-                    //EL_A_Values = (IGMData_Mag_EL_A_Values)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[4, 0]).Data;
-                    //EL_D_Values = (IGMData_Mag_EL_D_Values)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[5, 0]).Data;
-                    //ST_A_Values = (IGMData_Mag_ST_A_Values)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[7, 0]).Data;
-                    //ST_D_Values = (IGMData_Mag_ST_D_Values)((IGMDataItem_IGMData)((IGMData_Mag_Group)InGameMenu_Junction.Data[SectionName.Mag_Group]).ITEM[8, 0]).Data;
                     Source = Memory.State.Characters[Character];
 
                 public void Get_Sort()
@@ -177,7 +141,6 @@ namespace FF8
                         case Mode.Mag_Pool_Stat:
                             if (Stat != Kernel_bin.Stat.None)
                                 Sort = Kernel_bin.MagicData.OrderBy(x => (-x.J_Val[Stat] * (Source.Magics.ContainsKey(x.ID) ? Source.Magics[x.ID] : 0)) / 100);
-                            //Kernel_bin.MagicData.ToList().ConvertAll(x => (x.J_Val[Stat] * (Source.Magics.ContainsKey(x.ID) ? Source.Magics[x.ID] : 0)) / 100);
                             break;
 
                         case Mode.Mag_Pool_EL_D:
@@ -210,26 +173,23 @@ namespace FF8
                         SlotUndoListener?.Invoke(this, InGameMenu_Junction.GetMode());
                         SlotConfirmListener?.Invoke(this, InGameMenu_Junction.GetMode());
                         SlotReinitListener?.Invoke(this, InGameMenu_Junction.GetMode());
-                        if (InGameMenu_Junction.GetMode() == Mode.Mag_Pool_Stat)
+                        switch(SortMode)
                         {
-                            InGameMenu_Junction.SetMode(Mode.Mag_Stat);
-                            //Stat_Slots.UndoChange();
-                            //Stat_Slots.ConfirmChange();
-                            //Stat_Slots.ReInit();
-                        }
-                        else if (SortMode == Mode.Mag_Pool_EL_A || SortMode == Mode.Mag_Pool_EL_D)
-                        {
-                            InGameMenu_Junction.SetMode(Mode.Mag_EL_A);
-                            //EL_A_D_Slots.UndoChange();
-                            //EL_A_D_Slots.ConfirmChange();
-                            //EL_A_D_Slots.ReInit();
-                        }
-                        else if (SortMode == Mode.Mag_Pool_ST_A || SortMode == Mode.Mag_Pool_ST_D)
-                        {
-                            InGameMenu_Junction.SetMode(Mode.Mag_ST_A);
-                            //ST_A_D_Slots.UndoChange();
-                            //ST_A_D_Slots.ConfirmChange();
-                            //ST_A_D_Slots.ReInit();
+                            case Mode.Mag_Pool_Stat:
+                                InGameMenu_Junction.SetMode(Mode.Mag_Stat);
+                                break;
+                            case Mode.Mag_Pool_EL_A:
+                                InGameMenu_Junction.SetMode(Mode.Mag_EL_A);
+                                break;
+                            case Mode.Mag_Pool_EL_D:
+                                InGameMenu_Junction.SetMode(Mode.Mag_EL_D);
+                                break;
+                            case Mode.Mag_Pool_ST_A:
+                                InGameMenu_Junction.SetMode(Mode.Mag_ST_A);
+                                break;
+                            case Mode.Mag_Pool_ST_D:
+                                InGameMenu_Junction.SetMode(Mode.Mag_ST_D);
+                                break;
                         }
 
                         Cursor_Status &= ~Cursor_Status.Enabled;
@@ -405,6 +365,14 @@ namespace FF8
 
                 private void StatChangeEvent(object sender, Kernel_bin.Stat e) => UpdateOnEvent(sender, null, e);
 
+                private bool Undo()
+                {
+                    SlotUndoListener?.Invoke(this, InGameMenu_Junction.GetMode());
+                    if (Memory.State.Characters != null)
+                        Source = Memory.State.Characters[Character];
+                    return true;
+                }
+
                 private void UpdateOnEvent(object sender, Mode? mode = null, Kernel_bin.Stat? stat = null)
                 {
                     mode = mode ?? InGameMenu_Junction.GetMode();
@@ -428,13 +396,14 @@ namespace FF8
                         Get_Slots_Values();
                         if (SortMode != LastMode || this.Stat != LastStat || Character != LastCharacter)
                             Get_Sort();
-
+                        bool skipundo = false;
                         if (!(SortMode == LastMode && Character == LastCharacter && this.Stat == LastStat && Page == LastPage))
                         {
                             LastCharacter = Character;
                             LastStat = this.Stat;
                             LastPage = Page;
                             LastMode = SortMode;
+                            skipundo = Undo();
                             FillMagic();
                             UpdateTitle();
                         }
@@ -445,8 +414,7 @@ namespace FF8
                             mode == Mode.Mag_Pool_EL_D ||
                             mode == Mode.Mag_Pool_Stat)
                         {
-                            Get_Current_Slot_Value();
-                            Generate_Preview();
+                            Generate_Preview(skipundo);
                         }
                     }
                 }
@@ -455,8 +423,10 @@ namespace FF8
             }
 
             #endregion Classes
+
         }
 
         #endregion Classes
+
     }
 }
