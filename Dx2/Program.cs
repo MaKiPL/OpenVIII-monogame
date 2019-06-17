ac3a3df69108fc6edf54b5234eaeacd0a670a412
Sebanisu <peeceseven@gmail.com>
2019-06-17 15:42:05 -0400

    DirectX build in testing

---
+++ Dx2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FF8;
+
+namespace Dx2
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
