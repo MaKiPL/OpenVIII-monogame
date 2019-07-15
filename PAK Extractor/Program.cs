ba74e7cfd6d26bdf60967fb4042517a0a406aa73
Sebanisu <peeceseven@gmail.com>
2019-07-15 14:26:28 -0400

    Saves added ignore case
    Simplified the build path for multiple projects. to bin\ we had set this before somehow they were unset.
    
    Added PAK extractor project.
    
    Added a Tools solution folder.

---
+++ PAK Extractor/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OpenVIII.PAK_Extractor
+{
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            Input.Init();
+            Memory.Init(null, null, null);
+start:
+            List<FileInfo> files = new List<FileInfo>(1);
+            List<PAK> data = new List<PAK>(1);
+            Console.WriteLine($"This tool is used to extract the movies from the FF8 CDs, to be more like the steam version.");
+            Console.WriteLine($"Detecting Compact Disc drives...");
+            foreach (DriveInfo drive in DriveInfo.GetDrives()
+                               .Where(d => d.DriveType == DriveType.CDRom && d.IsReady))
+            {                
+                DirectoryInfo dir = drive.RootDirectory;
+
+                Console.WriteLine($"Found disc: {dir}");
+
+                Console.WriteLine($"Scanning for PAK files...");
+                files.AddRange(dir.GetFiles("*", SearchOption.TopDirectoryOnly).Where(x =>
+                  x.FullName.EndsWith(".pak", StringComparison.OrdinalIgnoreCase)));
+            }
+
+            string DestPath = Path.Combine(Memory.FF8DIRdata, "movies");
+            //DestPath = Path.Combine(Path.GetTempPath(), "movies"); //comment out to default to ff8 folder.
+            foreach (FileInfo f in files)
+            {
+                Console.WriteLine($"PAK file detected: {f}");
+                PAK pak = new PAK(f);
+                long SumHI = pak.Movies.Sum(g => g.BINK_HIGH.Size);
+                long SumLO = pak.Movies.Sum(g => g.BINK_LOW.Size);
+                long SumCA = pak.Movies.Sum(g => g.CAM.Size);
+                Console.WriteLine($"PAK has {pak.Count} videos. Total of {SumHI / 1024} KB (HI Res BINK), {SumLO / 1024} KB (LOW Res BINK), {SumCA / 1024} KB (CAM)");
+                data.Add(pak);
+            }
+
+            if (data.Count > 0)
+            {
+                Console.WriteLine($"Destination: {DestPath}\nPress [Enter] to continue. If you'd like to override this path, type a new destination, and then press [ENTER].");
+                string input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                    DestPath = input;
+                if (!Directory.Exists(DestPath)) Directory.CreateDirectory(DestPath);
+
+                foreach (PAK pak in data)
+                {
+                    pak.Extract(DestPath);
+                    
+                }
+            }
+            
+            Console.WriteLine($"Insert disc and press [Enter].");
+            Console.ReadLine();
+            goto start;
+        }
+
+        #endregion Methods
+    }
+}
