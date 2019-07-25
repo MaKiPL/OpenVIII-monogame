d94f99dc2ec61112dfe64262d3850b8d2db0334c
Sebanisu <peeceseven@gmail.com>
2019-07-25 11:33:13 -0400

    Update OrderedDictionary.cs
    Removed dictionary. Made List a list of key value pairs. now inherits Idictionary<>

--- Core/Field/Framework/OrderedDictionary.cs
+++ Core/Field/Framework/OrderedDictionary.cs
@@ -1,41 +1,177 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenVIII
 {
-    public sealed class OrderedDictionary<TKey, TValue>
+    /// <summary>
+    /// <para>List of KeyValuePairs</para>
+    /// <para>TKey cannot be int use a List</para>
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public sealed class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
-        private readonly List<TValue> _list;
-        private readonly Dictionary<TKey, TValue> _dic;
+        #region Fields
 
-        public OrderedDictionary()
+        private List<KeyValuePair<TKey, TValue>> _list;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public OrderedDictionary(int capacity = 0) => _list = new List<KeyValuePair<TKey, TValue>>(capacity);
+
+        #endregion Constructors
+
+        #region Properties
+
+        public bool IsFixedSize => false;
+        public bool IsReadOnly => false;
+        public bool IsSynchronized => false;
+        public IReadOnlyList<TKey> Keys => _list.Select(kvp => kvp.Key).ToList();
+        public IReadOnlyList<TValue> Values => _list.Select(kvp => kvp.Value).ToList();
+        public int Count => _list.Count;
+
+        ICollection<TKey> IDictionary<TKey, TValue>.Keys => _list.Select(kvp => kvp.Key).ToArray();
+
+        ICollection<TValue> IDictionary<TKey, TValue>.Values => _list.Select(kvp => kvp.Value).ToArray();
+
+        #endregion Properties
+
+        #region Indexers
+
+        public KeyValuePair<TKey, TValue> this[int index]
         {
-            _list = new List<TValue>();
-            _dic = new Dictionary<TKey, TValue>();
+            get
+            {
+                if (TryGetKVPByIndex(index, out KeyValuePair<TKey, TValue> kvp))
+                    return kvp;
+                throw new KeyNotFoundException($"{this}::Index:{index} - Not Found!");
+            }
         }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (TryGetByKey(key, out TValue value))
+                    return value;
+                throw new KeyNotFoundException($"{this}::Key:{key} - Not Found!");
+            }
+            set
+            {
+                int index = _list.FindIndex(kvp => kvp.Key.Equals(key));
+                if(index>=0)
+                    _list[index] = new KeyValuePair<TKey, TValue>(key, value);
+                else
+                    throw new KeyNotFoundException($"{this}::Key:{key} - Not Found!");
+            }
+        }
+
+        TValue IDictionary<TKey, TValue>.this[TKey key]
+        {
+            get
+            {
+                return this[key];
+            }
+            set
+            {
+                this[key] = value;
+            }
+        }
+
+        #endregion Indexers
+
+        #region Methods
 
         public void Add(TKey key, TValue value)
         {
-            _dic.Add(key, value);
-            _list.Add(value);
+            if (!TryAdd(key, value)) throw new ArgumentException($"{this}::Key:{key} - Already Exists!");
         }
 
-        public Boolean ContainsKey(TKey key)
+        public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
+
+        public void Clear() => _list.Clear();
+
+        public bool Contains(KeyValuePair<TKey, TValue> item) => _list.Contains(item);
+
+        public Boolean ContainsIndex(Int32 index) => index < _list.Count;
+
+        public Boolean ContainsKey(TKey key) => _list.Any(kvp => kvp.Key.Equals(key));
+
+        public Boolean ContainsValue(TValue value) => _list.Any(kvp => kvp.Value.Equals(value));
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
-            return _dic.ContainsKey(key);
+            if (Count <= array.Length + arrayIndex)
+            {
+                foreach (KeyValuePair<TKey, TValue> i in _list)
+                {
+                    array[arrayIndex++] = i;
+                }
+                return;
+            }
+            throw new OverflowException($"{this}::Count:{Count} > array.Length:{array.Length} + arrayIndex:{arrayIndex}");
         }
 
-        public Boolean ContainsIndex(Int32 index)
+        public IEnumerator GetEnumerator() => _list.GetEnumerator();
+
+        public bool Remove(TKey key)
         {
-            return index < _list.Count;
+            KeyValuePair<TKey, TValue> kvp = _list.First(_kvp => _kvp.Key.Equals(key));
+            return _list.Remove(kvp);
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item) => _list.Remove(item);
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            if (!ContainsKey(key))
+            {
+                _list.Add(new KeyValuePair<TKey, TValue>(key, value));
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean TryGetByIndex(Int32 index, out TValue value)
+        {
+            if (ContainsIndex(index))
+            {
+                value = _list[index].Value;
+                return true;
+            }
+
+            value = default;
+            return false;
         }
 
         public Boolean TryGetByKey(TKey key, out TValue value)
         {
-            return _dic.TryGetValue(key, out value);
+            if (ContainsKey(key))
+            {
+                value = _list.First(kvp => kvp.Key.Equals(key)).Value;
+                return true;
+            }
+            value = default;
+            return false;
         }
 
-        public Boolean TryGetByIndex(Int32 index, out TValue value)
+        public Boolean TryGetKeyByIndex(Int32 index, out TKey value)
+        {
+            if (ContainsIndex(index))
+            {
+                value = _list[index].Key;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        public Boolean TryGetKVPByIndex(Int32 index, out KeyValuePair<TKey, TValue> value)
         {
             if (ContainsIndex(index))
             {
@@ -43,17 +179,14 @@ namespace OpenVIII
                 return true;
             }
 
-            value = default(TValue);
+            value = default;
             return false;
         }
 
-        public IReadOnlyList<TValue> Values => _list;
+        public bool TryGetValue(TKey key, out TValue value) => TryGetByKey(key, out value);
 
-        public void Clear()
-        {
-            _dic.Clear();
-            _list.Clear();
-        }
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() => _list.GetEnumerator();
+
+        #endregion Methods
     }
 }
-
