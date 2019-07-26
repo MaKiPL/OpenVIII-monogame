355ec80d456386f1d56d5a485cbec050a76f5329
Sebanisu <peeceseven@gmail.com>
2019-07-25 20:43:35 -0400

    Move Input to folder and broke out enums to their own files

---
+++ Core/Input/Buttons.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenVIII
+{
+    public enum Buttons
+    {
+        None, Up, Down, Left, Right, Okay, Cancel, Exit, Switch, Menu, Back, Start, X, Y, B, A, L1, L2, L3, R1, R2, R3, Select,
+        LeftStickX, LeftStickY, RightStickX, RightStickY,
+        MouseX, MouseY, MouseXjoy, MouseYjoy,
+        MouseLeft, MouseMiddle, MouseRight, Mouse4, Mouse5, MouseWheelup, MouseWheeldown,
+        Triangle = Y, Square = X, Circle = B, Cross = A
+    }
+}
---
+++ Core/Input/MouseLockMode.cs
@@ -0,0 +1,20 @@
+﻿namespace OpenVIII
+{
+
+    public partial class Input
+    {
+        #endregion Fields
+
+        #region Enums
+
+        public enum MouseLockMode
+        {
+            Center,
+            Screen
+        }
+
+        #endregion Methods
+
+        // fail over to IsPressed
+    }
+}
