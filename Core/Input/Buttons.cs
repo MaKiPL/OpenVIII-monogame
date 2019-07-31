920157ad29dbc95876164748539713a389e3ab64
Sebanisu <peeceseven@gmail.com>
2019-07-31 12:30:32 -0400

    Update Buttons.cs
    put controller only buttons into their own enum and mouse buttons into their own enum.

--- Core/Input/Buttons.cs
+++ Core/Input/Buttons.cs
@@ -8,4 +8,15 @@
         MouseLeft, MouseMiddle, MouseRight, Mouse4, Mouse5, MouseWheelup, MouseWheeldown,
         Triangle = Y, Square = X, Circle = B, Cross = A
     }
+    public enum ControllerButtons
+    {
+        None, Up, Down, Left, Right, Back, Start, X, Y, B, A, L1, L2, L3, R1, R2, R3, 
+        LeftStickX, LeftStickY, RightStickX, RightStickY,
+        Select = Back, Triangle = Y, Square = X, Circle = B, Cross = A
+    }
+    public enum MouseButtons
+    {
+        None, MouseX, MouseY, MouseXjoy, MouseYjoy,
+        MouseLeft, MouseMiddle, MouseRight, Mouse4, Mouse5, MouseWheelup, MouseWheeldown,
+    }
 }
