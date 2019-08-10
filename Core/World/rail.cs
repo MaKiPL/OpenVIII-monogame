e54de7a606db94f21de914d9d4baec7df2104130
Marcin Gomulak <makipol@gmail.com>
2019-08-10 16:19:37 +0200

    reverting memory + adding rail which I forgot

---
+++ Core/World/rail.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVIII.Core.World
+{
+    /// <summary>
+    /// Rail.obj is a file that contains waypoints and stops for trains including sequences
+    /// </summary>
+    class rail
+    {
+        private const int RAIL_BLOCK_SIZE = 2048;
+        private const int BLOCK_HEADER_SIZE = 12;
+
+        [StructLayout(LayoutKind.Sequential, Pack =1, Size =8)]
+        private struct RailEntry
+        {
+            public byte cKeypoints;
+            public byte unk;
+            public ushort unk2;
+            public uint trainStop1;
+            public uint trainStop2;
+            public Keypoint[] keypoints;
+
+            public Keypoint TrainStop1 => keypoints[trainStop1];
+            public Keypoint TrainStop2 => keypoints[trainStop2];
+        }
+        /// <summary>
+        /// to reverse, but probably should be finally changed to vec4- it's normal vector, but relative to world coordinates of vanilla world map
+        /// </summary>
+        /// 
+        [StructLayout(LayoutKind.Sequential, Pack =1, Size =16)]
+        private struct Keypoint
+        {
+            public int x;
+            public int z;
+            public int y;
+            public int padd;
+        }
+
+        RailEntry[] railEntries;
+
+        public rail(byte[] buffer)
+        {
+            int railEntries = buffer.Length / RAIL_BLOCK_SIZE;
+            this.railEntries = new RailEntry[railEntries];
+            using(MemoryStream ms = new MemoryStream(buffer))
+            using (BinaryReader br = new BinaryReader(ms))
+                for (int i = 0; i < railEntries; i++)
+                    this.railEntries[i] = ParseBlock(br.ReadBytes(2048));
+
+        }
+
+        private RailEntry ParseBlock(byte[] block)
+        {
+            RailEntry entry = new RailEntry()
+            {
+                cKeypoints = block[0],
+                unk = block[1],
+                unk2 = BitConverter.ToUInt16(block, 2),
+                trainStop1 = BitConverter.ToUInt32(block, 4),
+                trainStop2 = BitConverter.ToUInt32(block, 8)
+            };
+            entry.keypoints = new Keypoint[entry.cKeypoints];
+            for(int i = 0; i<entry.cKeypoints; i++)
+                entry.keypoints[i] = Extended.ByteArrayToStructure<Keypoint>(block.Skip(BLOCK_HEADER_SIZE+(i*16)).Take(16).ToArray());
+            return entry;
+        }
+    }
+}
