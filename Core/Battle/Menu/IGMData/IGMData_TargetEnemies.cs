eba547055f26bb6942fb47a586f99cb787bb31fb
Sebanisu <peeceseven@gmail.com>
2019-08-21 09:18:20 -0400

    split up target window classes into their own files

---
+++ Core/Battle/Menu/IGMData/IGMData_TargetEnemies.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace OpenVIII
+{
+    public partial class BattleMenus
+    {
+        #endregion Methods
+        #region Classes
+
+        public class IGMData_TargetEnemies : IGMData
+        {
+            #region Constructors
+
+            public IGMData_TargetEnemies(Rectangle pos) : base(6, 1, new IGMDataItem_Box(pos: pos, title: Icons.ID.TARGET), 2, 3)
+            {
+            }
+
+            #endregion Constructors
+
+            #region Methods
+
+            public override void Refresh()
+            {
+                if (Memory.State?.Characters != null)
+                {
+                    int pos = 0;
+                    if (Enemy.EnemyParty != null)
+                    {
+                        foreach (Enemy e in Enemy.EnemyParty)
+                        {
+                            //if(e.EII)
+                            ITEM[pos, 0] = new IGMDataItem_String(e.Name, SIZE[pos], Font.ColorID.White);
+                            ITEM[pos, 0].Show();
+                            BLANKS[pos] = false;
+
+                            pos++;
+                        }
+                        for (; pos < Count; pos++)
+                        {
+                            ITEM[pos, 0]?.Hide();
+                            BLANKS[pos] = true;
+                        }
+                    }
+                }
+            }
+
+            public override void Inputs_Left()
+            {
+                if (CURSOR_SELECT < 3)
+                {
+                    Cursor_Status &= ~Cursor_Status.Enabled;
+                    Menu.BattleMenus.Target_Party.Cursor_Status |= Cursor_Status.Enabled;
+                    Menu.BattleMenus.Target_Party.CURSOR_SELECT = CURSOR_SELECT % 3;
+                }
+                else
+                {
+                    SetCursor_select(CURSOR_SELECT - 3);
+                }
+                base.Inputs_Left();
+            }
+            public override void Inputs_Right()
+            {
+                if (CURSOR_SELECT >= 3 || (ITEM[3, 0] == null || !ITEM[3, 0].Enabled))
+                {
+                    Cursor_Status &= ~Cursor_Status.Enabled;
+                    Menu.BattleMenus.Target_Party.Cursor_Status |= Cursor_Status.Enabled;
+                    Menu.BattleMenus.Target_Party.CURSOR_SELECT = CURSOR_SELECT % 3;
+                }
+                else
+                {
+                    SetCursor_select(CURSOR_SELECT + 3);
+                }
+                base.Inputs_Right();
+            }
+
+            protected override void InitShift(int i, int col, int row)
+            {
+                base.InitShift(i, col, row);
+                SIZE[i].Inflate(-18, -20);
+                SIZE[i].Y -= 7 * row + 2;
+                //SIZE[i].Inflate(-22, -8);
+                //SIZE[i].Offset(0, 12 + (-8 * row));
+                SIZE[i].Height = (int)(12 * TextScale.Y);
+            }
+
+            #endregion Methods
+        }
+
+        #endregion Classes
+    }
+}
---
+++ Core/Battle/Menu/IGMData/IGMData_TargetGroup.cs
@@ -0,0 +1,50 @@
+﻿namespace OpenVIII
+{
+    public partial class BattleMenus
+    {
+        #endregion Methods
+
+        #region Classes
+
+        public class IGMData_TargetGroup : IGMData_Group
+        {
+            public IGMData_TargetGroup(params IGMData[] d) : base(d)
+            {
+            }
+
+            public override bool Inputs()
+            {
+                IGMData_TargetEnemies i = (IGMData_TargetEnemies)(((IGMDataItem_IGMData)ITEM[0, 0]).Data);
+                IGMData_TargetParty ii = (IGMData_TargetParty)(((IGMDataItem_IGMData)ITEM[1, 0]).Data);
+                bool ret = false;
+                if (i.Enabled && (((i.Cursor_Status | ii.Cursor_Status) & Cursor_Status.Enabled) == 0 || !ii.Enabled))
+                    i.Cursor_Status |= Cursor_Status.Enabled;
+                else if (ii.Enabled && (((i.Cursor_Status | ii.Cursor_Status) & Cursor_Status.Enabled) == 0 || !i.Enabled))
+                    ii.Cursor_Status |= Cursor_Status.Enabled;
+                if (i.Enabled && (i.Cursor_Status & Cursor_Status.Enabled) != 0)
+                {
+                    ret = i.Inputs();
+                }
+                else if (ii.Enabled && (ii.Cursor_Status & Cursor_Status.Enabled) != 0)
+                {
+                    ret = ii.Inputs() || ret;
+                }
+                if(!ret)
+                {
+                    Cursor_Status = Cursor_Status.Hidden | Cursor_Status.Static | Cursor_Status.Enabled;
+                    skipdata = true;
+                    ret = base.Inputs();
+                    skipdata = false;
+                }
+                return ret;
+            }
+            public override bool Inputs_CANCEL()
+            {
+                Hide();
+                return base.Inputs_CANCEL();
+            }
+        }
+
+        #endregion Classes
+    }
+}
