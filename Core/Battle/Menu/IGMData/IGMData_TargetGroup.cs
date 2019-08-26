7e96da2475840c2976fa3d261fa3992298db84a6
Sebanisu <peeceseven@gmail.com>
2019-08-26 14:31:05 -0400

    Update IGMData_TargetGroup.cs
    Rewrote the switch as a dictionary.  prepopulated it

--- Core/Battle/Menu/IGMData/IGMData_TargetGroup.cs
+++ Core/Battle/Menu/IGMData/IGMData_TargetGroup.cs
@@ -8,32 +8,30 @@ namespace OpenVIII
 {
     public partial class BattleMenus
     {
+        #region Methods
+
+        private void DrawMagic(Debug_battleDat.Magic[] drawList) =>
+            // display pool with list.
+            Debug.WriteLine($"Display draw pool: {string.Join(", ", drawList)}");
+
+        #endregion Methods
+
         #region Classes
 
         public class IGMData_TargetGroup : IGMData_Group
         {
-            public Kernel_bin.Battle_Commands Command { get; private set; }
-            public Kernel_bin.Blue_magic_Quistis_limit_break BlueMagic { get; private set; }
-            public Kernel_bin.Magic_Data Magic { get; private set; }
+            #region Fields
+
+            private readonly int[] Renzokuken_hits = { 4, 5, 6, 7 };
+            private Dictionary<int, Func<bool>> CommandFunc;
+
+            #endregion Fields
 
             #region Constructors
 
-            public IGMData_TargetGroup(params IGMData[] d) : base(d)
-            {
-                after();
-            }
+            public IGMData_TargetGroup(params IGMData[] d) : base(d) => after();
 
-            private void after()
-            {
-                IGMData_TargetEnemies i = (IGMData_TargetEnemies)(((IGMDataItem_IGMData)ITEM[0, 0]).Data);
-                IGMData_TargetParty ii = (IGMData_TargetParty)(((IGMDataItem_IGMData)ITEM[1, 0]).Data);
-                i.Target_Party = ii;
-                ii.Target_Enemies = i;
-                Hide();
-            }
-            public override void Show() => base.Show();
-
-            public IGMData_TargetGroup():base()
+            public IGMData_TargetGroup() : base()
             {
                 const int X = 25;
                 const int w = 380;
@@ -42,10 +40,19 @@ namespace OpenVIII
                 const int Y1 = 630 - h;
                 CONTAINER.Pos = new Rectangle(X, Y1, w + w2, h);
                 Init(new IGMData[]{ new IGMData_TargetEnemies(new Rectangle(CONTAINER.Pos.X, CONTAINER.Pos.Y, w, h)),
-                        new IGMData_TargetParty(new Rectangle(CONTAINER.Pos.X + w, CONTAINER.Pos.Y, w2, h)) },true);
+                        new IGMData_TargetParty(new Rectangle(CONTAINER.Pos.X + w, CONTAINER.Pos.Y, w2, h)) }, true);
                 after();
             }
+
             #endregion Constructors
+
+            #region Properties
+
+            public Kernel_bin.Blue_magic_Quistis_limit_break BlueMagic { get; private set; }
+            public Kernel_bin.Battle_Commands Command { get; private set; }
+            public Kernel_bin.Magic_Data Magic { get; private set; }
+
+            #endregion Properties
 
             #region Methods
 
@@ -81,12 +88,19 @@ namespace OpenVIII
                 return ret;
             }
 
-            public void ShowTargetWindows()
+            public override bool Inputs_CANCEL()
             {
-                skipdata = true;
-                Show();
-                skipdata = false;
-                Refresh();
+                Hide();
+                return true;
+            }
+
+            public override bool Inputs_OKAY()
+            {
+                base.Inputs_OKAY();
+                if (CommandFunc.TryGetValue(Command.ID, out Func<bool> val))
+                    return val();
+                else
+                    return CommandDefault();
             }
 
             public void SelectTargetWindows(Kernel_bin.Battle_Commands c)
@@ -116,6 +130,219 @@ namespace OpenVIII
                 BlueMagic = c;
             }
 
+            public override void Show() => base.Show();
+
+            public void ShowTargetWindows()
+            {
+                skipdata = true;
+                Show();
+                skipdata = false;
+                Refresh();
+            }
+
+            protected override void Init()
+            {
+                if (CommandFunc == null)
+                    CommandFunc = new Dictionary<int, Func<bool>>
+                    {
+                        {0,Command00 },
+                        {1,Command01_ATTACK },
+                        {2,Command02_MAGIC },
+                        {3,Command03 },
+                        {4,Command04 },
+                        {5,Command05_RENZOKUKEN },
+                        {6,Command06_DRAW },
+                        {7,Command07 },
+                        {8,Command08 },
+                        {9,Command09 },
+                        {10,Command10 },
+                        {11,Command11 },
+                        {12,Command12 },
+                        {13,Command13 },
+                        {14,Command14 },
+                        {15,Command15 },
+                        {16,Command16 },
+                        {17,Command17 },
+                        {18,Command18 },
+                        {19,Command19 },
+                        {20,Command20 },
+                        {21,Command21 },
+                        {22,Command22 },
+                        {23,Command23 },
+                        {24,Command24 },
+                        {25,Command25 },
+                        {26,Command26 },
+                        {27,Command27 },
+                        {28,Command28 },
+                        {29,Command29 },
+                        {30,Command30 },
+                        {31,Command31 },
+                        {32,Command32 },
+                        {33,Command33 },
+                        {34,Command34 },
+                        {35,Command35 },
+                        {36,Command36 },
+                        {37,Command37 },
+                        {38,Command38 },
+                        {39,Command39 },
+                    };
+                base.Init();
+            }
+
+            private void after()
+            {
+                IGMData_TargetEnemies i = (IGMData_TargetEnemies)(((IGMDataItem_IGMData)ITEM[0, 0]).Data);
+                IGMData_TargetParty ii = (IGMData_TargetParty)(((IGMDataItem_IGMData)ITEM[1, 0]).Data);
+                i.Target_Party = ii;
+                ii.Target_Enemies = i;
+                Hide();
+            }
+
+            private bool Command00() => throw new NotImplementedException();
+
+            private bool Command01_ATTACK()
+            {
+                Neededvaribles(out IGMData_TargetEnemies i, out Enemy e, out Characters vc, out Characters fromvc, out bool enemytarget);
+                return true;
+            }
+
+            private bool Command02_MAGIC()
+            {
+                Neededvaribles(out IGMData_TargetEnemies i, out Enemy e, out Characters vc, out Characters fromvc, out bool enemytarget);
+                Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} casts {Magic.Name}({Magic.ID}) spell on { (enemytarget ? $"{e.Name.Value_str}({i.CURSOR_SELECT})" : Memory.Strings.GetName(vc).Value_str) }");
+                return true;
+            }
+
+            private bool Command03() => throw new NotImplementedException();
+
+            private bool Command04() => throw new NotImplementedException();
+
+            private bool Command05_RENZOKUKEN()
+            {
+                Neededvaribles(out IGMData_TargetEnemies i, out Enemy e, out Characters vc, out Characters fromvc, out bool enemytarget);
+                //Renzokuken
+                byte w = Memory.State[fromvc].WeaponID;
+                int hits = Memory.State[fromvc].CurrentCrisisLevel < Renzokuken_hits.Length ? Renzokuken_hits[Memory.State[fromvc].CurrentCrisisLevel] : Renzokuken_hits.Last();
+                int finisherchance = (Memory.State[fromvc].CurrentCrisisLevel + 1) * 60;
+                bool willfinish = Memory.Random.Next(0, byte.MaxValue) <= finisherchance;
+                int choosefinish = Memory.Random.Next(0, 3);
+                Kernel_bin.Weapons_Data wd = Kernel_bin.WeaponsData[w];
+                Kernel_bin.Renzokeken_Finisher r = wd.Renzokuken;
+                List<Kernel_bin.Renzokeken_Finisher> flags = Enum.GetValues(typeof(Kernel_bin.Renzokeken_Finisher))
+                    .Cast<Kernel_bin.Renzokeken_Finisher>()
+                    .Where(f => (f & r) != 0)
+                    .ToList();
+                Kernel_bin.Renzokeken_Finisher finisher = choosefinish >= flags.Count ? flags.Last() : flags[choosefinish];
+                //per wiki the chance of which finisher is 25% each and the highest value finisher get the remaining of 100 percent.
+                //so rough divide is 100% when you only only have that
+                //when you unlock 2 one is 75% chance
+                //when you onlock 3 last one is 50%
+                //when you unlock all 4 it's 25% each.
+
+                //finishers each have their own target
+                if (willfinish)
+                    Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} hits {hits} times with {Command.Name}({Command.ID}) then uses {Kernel_bin.RenzokukenFinishersData[finisher].Name}.");
+
+                if (!willfinish)
+                    Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} hits {hits} times with {Command.Name}({Command.ID}) then fails to use a finisher.");
+                return true;
+            }
+
+            private bool Command06_DRAW()
+            {
+                Neededvaribles(out IGMData_TargetEnemies i, out Enemy e, out Characters vc, out Characters fromvc, out bool enemytarget);
+                //draw
+                //spawn a 1 page 4 row pool of the magic/gfs that the selected enemy has.
+                BattleMenus.DrawMagic(e.DrawList);
+                return true;
+            }
+
+            private bool Command07() => throw new NotImplementedException();
+
+            private bool Command08() => throw new NotImplementedException();
+
+            private bool Command09() => throw new NotImplementedException();
+
+            private bool Command10() => throw new NotImplementedException();
+
+            private bool Command11() => throw new NotImplementedException();
+
+            private bool Command12() => throw new NotImplementedException();
+
+            private bool Command13() => throw new NotImplementedException();
+
+            private bool Command14() => throw new NotImplementedException();
+
+            private bool Command15() => throw new NotImplementedException();
+
+            private bool Command16() => throw new NotImplementedException();
+
+            private bool Command17() => throw new NotImplementedException();
+
+            private bool Command18() => throw new NotImplementedException();
+
+            private bool Command19() => throw new NotImplementedException();
+
+            private bool Command20() => throw new NotImplementedException();
+
+            private bool Command21() => throw new NotImplementedException();
+
+            private bool Command22() => throw new NotImplementedException();
+
+            private bool Command23() => throw new NotImplementedException();
+
+            private bool Command24() => throw new NotImplementedException();
+
+            private bool Command25() => throw new NotImplementedException();
+
+            private bool Command26() => throw new NotImplementedException();
+
+            private bool Command27() => throw new NotImplementedException();
+
+            private bool Command28() => throw new NotImplementedException();
+
+            private bool Command29() => throw new NotImplementedException();
+
+            private bool Command30() => throw new NotImplementedException();
+
+            private bool Command31() => throw new NotImplementedException();
+
+            private bool Command32() => throw new NotImplementedException();
+
+            private bool Command33() => throw new NotImplementedException();
+
+            private bool Command34() => throw new NotImplementedException();
+
+            private bool Command35() => throw new NotImplementedException();
+
+            private bool Command36() => throw new NotImplementedException();
+
+            private bool Command37() => throw new NotImplementedException();
+
+            private bool Command38() => throw new NotImplementedException();
+
+            private bool Command39() => throw new NotImplementedException();
+
+            private bool CommandDefault() => throw new NotImplementedException();
+
+            private void DebugMessageCommand(IGMData_TargetEnemies i, Enemy e, Characters vc, Characters fromvc, bool enemytarget) => Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} uses {Command.Name}({Command.ID}) command on { (enemytarget ? $"{e.Name.Value_str}({i.CURSOR_SELECT})" : Memory.Strings.GetName(vc).Value_str) }");
+
+            private void Neededvaribles(out IGMData_TargetEnemies i, out Enemy e, out Characters vc, out Characters fromvc, out bool enemytarget)
+            {
+                i = (IGMData_TargetEnemies)(((IGMDataItem_IGMData)ITEM[0, 0]).Data);
+                IGMData_TargetParty ii = (IGMData_TargetParty)(((IGMDataItem_IGMData)ITEM[1, 0]).Data);
+                e = Enemy.Party[ii.CURSOR_SELECT < Enemy.Party.Count ? ii.CURSOR_SELECT : Enemy.Party.Count - 1];
+                Characters c = Memory.State.PartyData.Where(x => x != Characters.Blank).ToList()[ii.CURSOR_SELECT];
+                vc = Memory.State.Party.Where(x => x != Characters.Blank).ToList()[ii.CURSOR_SELECT];
+                int p = BattleMenus.Player;
+                Characters fromc = Memory.State.PartyData.Where(x => x != Characters.Blank).ToList()[p];
+                fromvc = Memory.State.Party.Where(x => x != Characters.Blank).ToList()[p];
+                enemytarget = false;
+                if ((i.Cursor_Status & Cursor_Status.Enabled) != 0 && i.Enabled)
+                    enemytarget = true;
+                DebugMessageCommand(i, e, vc, fromvc, enemytarget);
+            }
+
             private void SelectTargetWindows(Kernel_bin.Target t)
             {
                 IGMData_TargetEnemies i = (IGMData_TargetEnemies)(((IGMDataItem_IGMData)ITEM[0, 0]).Data);
@@ -130,84 +357,8 @@ namespace OpenVIII
                     i.Hide();
             }
 
-            public override bool Inputs_CANCEL()
-            {
-                Hide();
-                return true;
-            }
-
-            private readonly int[] Renzokuken_hits = { 4, 5, 6, 7 };
-
-            public override bool Inputs_OKAY()
-            {
-                //TODO improve this.
-                IGMData_TargetEnemies i = (IGMData_TargetEnemies)(((IGMDataItem_IGMData)ITEM[0, 0]).Data);
-                IGMData_TargetParty ii = (IGMData_TargetParty)(((IGMDataItem_IGMData)ITEM[1, 0]).Data);
-                Enemy e = Enemy.Party[ii.CURSOR_SELECT < Enemy.Party.Count ? ii.CURSOR_SELECT : Enemy.Party.Count-1 ];
-                Characters c = Memory.State.PartyData.Where(x => x != Characters.Blank).ToList()[ii.CURSOR_SELECT];
-                Characters vc = Memory.State.Party.Where(x => x != Characters.Blank).ToList()[ii.CURSOR_SELECT];
-                int p = BattleMenus.Player;
-                Characters fromc = Memory.State.PartyData.Where(x => x != Characters.Blank).ToList()[p];
-                Characters fromvc = Memory.State.Party.Where(x => x != Characters.Blank).ToList()[p];
-                bool enemytarget = false;
-                if ((i.Cursor_Status & Cursor_Status.Enabled) != 0 && i.Enabled)
-                    enemytarget = true;
-
-                Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} uses {Command.Name}({Command.ID}) command on { (enemytarget ? $"{e.Name.Value_str}({i.CURSOR_SELECT})" : Memory.Strings.GetName(vc).Value_str) }");
-                base.Inputs_OKAY();
-                //spawn sub menu
-                switch (Command.ID)
-                {
-                    case 1: //Attack
-                        break;
-                    case 2:
-                        Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} casts {Magic.Name}({Magic.ID}) spell on { (enemytarget ? $"{e.Name.Value_str}({i.CURSOR_SELECT})" : Memory.Strings.GetName(vc).Value_str) }");
-                        break;
-                    case 5: //Renzokuken
-                        byte w = Memory.State[fromvc].WeaponID;
-                        int hits = Memory.State[fromvc].CurrentCrisisLevel < Renzokuken_hits.Length ? Renzokuken_hits[Memory.State[fromvc].CurrentCrisisLevel] : Renzokuken_hits.Last();
-                        int finisherchance = (Memory.State[fromvc].CurrentCrisisLevel + 1) * 60;
-                        bool willfinish = Memory.Random.Next(0, byte.MaxValue) <= finisherchance;
-                        int choosefinish = Memory.Random.Next(0, 3);
-                        Kernel_bin.Weapons_Data wd = Kernel_bin.WeaponsData[w];
-                        Kernel_bin.Renzokeken_Finisher r = wd.Renzokuken;
-                        List<Kernel_bin.Renzokeken_Finisher> flags = Enum.GetValues(typeof(Kernel_bin.Renzokeken_Finisher))
-                            .Cast<Kernel_bin.Renzokeken_Finisher>()
-                            .Where(f => (f & r) != 0)
-                            .ToList();
-                        Kernel_bin.Renzokeken_Finisher finisher = choosefinish >= flags.Count ? flags.Last() : flags[choosefinish];
-                        //per wiki the chance of which finisher is 25% each and the highest value finisher get the remaining of 100 percent.
-                        //so rough divide is 100% when you only only have that
-                        //when you unlock 2 one is 75% chance
-                        //when you onlock 3 last one is 50%
-                        //when you unlock all 4 it's 25% each.
-
-                        //finishers each have their own target
-                        if (willfinish)
-                            Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} hits {hits} times with {Command.Name}({Command.ID}) then uses {Kernel_bin.RenzokukenFinishersData[finisher].Name}.");
-
-                        if (!willfinish)
-                            Debug.WriteLine($"{Memory.Strings.GetName(fromvc)} hits {hits} times with {Command.Name}({Command.ID}) then fails to use a finisher.");
-
-                        break;
-
-                    case 6: //draw
-                        //spawn a 1 page 4 row pool of the magic/gfs that the selected enemy has.
-                        BattleMenus.DrawMagic(e.DrawList);
-                        break;
-
-                    default:
-                        break;
-                }
-                return true;
-            }
-
             #endregion Methods
         }
-
-        private void DrawMagic(Debug_battleDat.Magic[] drawList) =>
-            // display pool with list.
-            Debug.WriteLine($"Display draw pool: {string.Join(", ", drawList)}");
 
         #endregion Classes
     }
