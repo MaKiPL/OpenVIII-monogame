332a0198b75c077314dd69f0346bcbde19e84a7d
Sebanisu <peeceseven@gmail.com>
2019-08-29 11:13:19 -0400

    refactor slide.cs
    Changed names of some values to be clearer. added properties to allow changing the end and start values.

--- Core/Slide.cs
+++ Core/Slide.cs
@@ -1,40 +1,63 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace OpenVIII
 {
     public class Slide<T>
     {
-        T _start;
-        T _end;
-        double _time;
-        double _currenttime;
-        Func<T, T, float, T> _function;
+        #region Fields
 
-        public Slide(T start, T end, double totaltime, Func<T, T, float, T> function)
+        private double _currentMS;
+        private T _end;
+        private Func<T, T, float, T> _function;
+        private T _start;
+        private double _totalMS;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Slide(T start, T end, double totalMS, Func<T, T, float, T> function)
         {
             _start = start;
             _end = end;
-            _time = totaltime;
+            _totalMS = totalMS;
             _function = function;
         }
 
-        public bool Done => _currenttime >= _time;
+        #endregion Constructors
+
+        #region Properties
+
+        public double CurrentMS => _currentMS;
+        public bool Done => _currentMS >= _totalMS;
+
+        public T End { get => _end; set => _end = value; }
+        public Func<T, T, float, T> Function { get => _function; set => _function = value; }
+        public T Start { get => _start; set => _start = value; }
+        public double TotalMS { get => _totalMS; set => _totalMS = value; }
+
+        #endregion Properties
+
+        #region Methods
+
+        public void Restart() => _currentMS = 0d;
+
+        public void Reverse()
+        {
+            T tmp = _start; _start = _end; _end = tmp;
+        }
+
         public T Update()
         {
             if (!Done)
             {
-                _currenttime += Memory.gameTime.ElapsedGameTime.TotalMilliseconds;
-                float percent = (float)(Done?1f:_currenttime / _time);
+                _currentMS += Memory.gameTime.ElapsedGameTime.TotalMilliseconds;
+                float percent = (float)(Done ? 1f : _currentMS / _totalMS);
                 return _function(_start, _end, percent);
             }
             return _end;
         }
-        public void Restart() => _currenttime = 0d;
-        public void Reverse() { T tmp = _start; _start = _end; _end = tmp; }
+
+        #endregion Methods
     }
 }
