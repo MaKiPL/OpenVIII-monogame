3d0af65e749ca603066f1cefd445255ebffd47f6
Robert Russell <peeceseven@gmail.com>
2019-09-19 08:43:23 -0400

    Update WindowsGameLocationProvider.cs
    refactored the detection of ff8 from windows. will detect steam2013 then pc2000 then remaster. remaster doesn't work yet. but had to find the reg key for another project so i added it in there.

--- Core/Sources/Environment/Game/Location/WindowsGameLocationProvider.cs
+++ Core/Sources/Environment/Game/Location/WindowsGameLocationProvider.cs
@@ -1,54 +1,75 @@
-﻿using System;
+﻿using Microsoft.Win32;
 using System.IO;
-using Microsoft.Win32;
 
 namespace OpenVIII
 {
     public sealed class WindowsGameLocationProvider : IGameLocationProvider
     {
-        private const String SteamRegistyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 39150";
-        private const String SteamGamePathTag = @"InstallLocation";
+        /// <summary>
+        /// Steam 2013
+        /// </summary>
+        private const string SteamRegistyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 39150";
+
+        /// <summary>
+        /// Steam 2013
+        /// </summary>
+        private const string SteamGamePathTag = @"InstallLocation";
+
         /// <summary>
         /// Supplied from LordUrQuan
         /// </summary>
-        private const String CD2000RegistyPath = @"SOFTWARE\Wow6432Node\Square Soft, Inc\FINAL FANTASY VIII\1.00";
-        private const String CD2000GamePathTag = @"AppPath";
+        private const string CD2000RegistyPath = @"SOFTWARE\Wow6432Node\Square Soft, Inc\FINAL FANTASY VIII\1.00";
+
+        /// <summary>
+        /// Supplied from LordUrQuan
+        /// </summary>
+        private const string CD2000GamePathTag = @"AppPath";
+
+        /// <summary>
+        /// ReMaster
+        /// </summary>
+        private const string SteamRERegistyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 1026680";
+
+        /// <summary>
+        /// ReMaster
+        /// </summary>
+        private const string SteamREGamePathTag = @"InstallLocation";
 
         public GameLocation GetGameLocation()
         {
-            if (_hardcoded.FindGameLocation(out var gameLocation))
+            if (_hardcoded.FindGameLocation(out GameLocation gameLocation))
                 return gameLocation;
 
             foreach (RegistryView registryView in new RegistryView[] { RegistryView.Registry32, RegistryView.Registry64 })
             {
-                using (RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
-                using (RegistryKey registryKey = localMachine.OpenSubKey(SteamRegistyPath))
-                {
-                    if (registryKey != null)
-                    {
-                        String installLocation = (String)registryKey.GetValue(SteamGamePathTag);
-                        String dataPath = installLocation;//Path.Combine(installLocation, "Data", "lang-en");
-                        if (Directory.Exists(dataPath))
-                            return new GameLocation(dataPath);
-                    }
-                }
-                using (RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
-                using (RegistryKey registryKey = localMachine.OpenSubKey(CD2000RegistyPath))
-                {
-                    if (registryKey != null)
-                    {
-                        String installLocation = (String)registryKey.GetValue(CD2000GamePathTag);
-                        String dataPath = installLocation; //Path.Combine(installLocation, "Data"); //no lang-en on cd version.
-                        if (Directory.Exists(dataPath))
-                            return new GameLocation(dataPath);
-                    }
-                }
+                GameLocation r;
+                if ((r = Check(SteamRegistyPath, SteamGamePathTag)) == null &&
+                    (r = Check(CD2000RegistyPath, CD2000RegistyPath)) == null &&
+                    (r = Check(SteamRERegistyPath, SteamREGamePathTag)) == null)
+                    continue;
+                else
+                    return r;
 
+                GameLocation Check(string path, string tag)
+                {
+                    using (RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+                    using (RegistryKey registryKey = localMachine.OpenSubKey(path))
+                    {
+                        if (registryKey != null)
+                        {
+                            string installLocation = (string)registryKey.GetValue(tag);
+                            string dataPath = installLocation;
+                            if (Directory.Exists(dataPath))
+                                return new GameLocation(dataPath);
+                        }
+                    }
+                    return null;
+                }
             }
 
-                throw new DirectoryNotFoundException($"Cannot find game directory." +
-                                                 $"Add your own path to the {nameof(WindowsGameLocationProvider)} type or fix a registration in the registry:" +
-                                                 $"{SteamRegistyPath}.{SteamGamePathTag}");
+            throw new DirectoryNotFoundException($"Cannot find game directory." +
+                                             $"Add your own path to the {nameof(WindowsGameLocationProvider)} type or fix a registration in the registry:" +
+                                             $"{SteamRegistyPath}.{SteamGamePathTag}");
         }
 
         private readonly HardcodedGameLocationProvider _hardcoded = new HardcodedGameLocationProvider(new[]
