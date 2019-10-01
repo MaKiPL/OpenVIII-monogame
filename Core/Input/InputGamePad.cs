de5ee729cbe8c912590acfe4c2b0696fccb5c922
Robert Russell <peeceseven@gmail.com>
2019-09-30 23:54:09 -0400

    Update InputGamePad.cs
    added isactive to translate stick

--- Core/Input/InputGamePad.cs
+++ Core/Input/InputGamePad.cs
@@ -8,107 +8,19 @@ namespace OpenVIII
 {
     public class InputGamePad : Input2
     {
-        protected struct Iap
-        {
-            public Icons.ID id;
-            public byte palette;
-
-            public Iap(Icons.ID id = Icons.ID.None, byte palette = 2)
-            {
-                this.id = id;
-                this.palette = palette;
-            }
-        }
         #region Fields
 
         private static readonly float deadzone_r = 1f / 100;
-        private static GamePadState last_state;
-        private static GamePadState state;
 
-        private static Dictionary<GamePadButtons,Iap> iap;
+        private static Dictionary<GamePadButtons, Iap> iap;
+
+        private static GamePadState last_state;
+
+        private static GamePadState state;
 
         #endregion Fields
 
-        #region Constructors
-
-        public InputGamePad(bool skip = true) : base(skip)
-        {
-            if(iap == null)
-            {
-                iap = new Dictionary<GamePadButtons, Iap>
-                {
-                    { GamePadButtons.Up, new Iap{ id= Icons.ID.D_Pad_Up, palette = 2 } },
-                    { GamePadButtons.Down, new Iap{ id= Icons.ID.D_Pad_Down, palette = 2 } },
-                    { GamePadButtons.Left, new Iap{ id= Icons.ID.D_Pad_Left, palette = 2 } },
-                    { GamePadButtons.Right, new Iap{ id= Icons.ID.D_Pad_Right, palette = 2 } },
-                    { GamePadButtons.X, new Iap {id = Icons.ID.Size_16x16_PSX_Square, palette = 4} },
-                    { GamePadButtons.Y, new Iap {id = Icons.ID.Size_16x16_PSX_Triangle, palette = 4} },
-                    { GamePadButtons.A, new Iap {id = Icons.ID.Size_16x16_PSX_Cross, palette = 4} },
-                    { GamePadButtons.B, new Iap {id = Icons.ID.Size_16x16_PSX_Circle, palette = 4} },
-                    { GamePadButtons.Back, new Iap {id = Icons.ID.SELECT, palette = 2} },
-                    { GamePadButtons.Start, new Iap {id = Icons.ID.START, palette = 2} },
-                    { GamePadButtons.Left_Shoulder, new Iap {id = Icons.ID.Size_16x08_PSX_L1, palette = 2} },
-                    { GamePadButtons.Right_Shoulder, new Iap {id = Icons.ID.Size_16x08_PSX_R1, palette = 2} },
-                    { GamePadButtons.Left_Trigger, new Iap {id = Icons.ID.Size_16x08_PSX_L2, palette = 2} },
-                    { GamePadButtons.Right_Trigger, new Iap {id = Icons.ID.Size_16x08_PSX_R2, palette = 2} },
-                };
-            }
-        }
-
-        #endregion Constructors
-
-        #region Properties
-
-        protected static GamePadState Last_State => last_state;
-        protected static GamePadState State
-        {
-            get => state; set
-            {
-                last_state = state;
-                state = value;
-            }
-        }
-
-        #endregion Properties
-
         #region Methods
-
-        public static Vector2 Distance(GamePadButtons stick, float speed)
-        {
-            return Translate_Stick(stick, state) * (float)Distance(speed);
-        }
-
-        public override bool ButtonTriggered(InputButton test, ButtonTrigger trigger = ButtonTrigger.None)
-        {
-            if (test != null && test.GamePadButton != GamePadButtons.None)
-            {
-                bool combotest = false;
-                if (test.Combo != null)
-                {
-                    foreach (var item in test.Combo)
-                    {
-                        item.Trigger = ButtonTrigger.Press;
-                        if(!base.ButtonTriggered(item))
-                        {
-                            return false;
-                        }
-                    }
-                    combotest = true;
-                }
-                var triggertest = test.Trigger | trigger;
-                return ((test.Combo == null || combotest) &&
-                    ((triggertest & ButtonTrigger.OnPress) != 0 && OnPress(test.GamePadButton)) ||
-                    ((triggertest & ButtonTrigger.OnRelease) != 0 && OnRelease(test.GamePadButton)) ||
-                    ((triggertest & ButtonTrigger.Press) != 0 && Press(test.GamePadButton, state)));
-            }
-            return false;
-        }
-
-        protected override bool UpdateOnce()
-        {
-            State = Microsoft.Xna.Framework.Input.GamePad.GetState(PlayerIndex.One, GamePadDeadZone.Circular);
-            return false;
-        }
 
         private static ButtonState Translate_Button(GamePadButtons k, GamePadState _state)
         {
@@ -161,16 +73,17 @@ namespace OpenVIII
 
         private static Vector2 Translate_Stick(GamePadButtons k, GamePadState _state)
         {
-            switch (k)
-            {
-                case GamePadButtons.ThumbSticks_Left:
-                case GamePadButtons.LeftStick:
-                    return _state.ThumbSticks.Left;
+            if (Memory.IsActive)
+                switch (k)
+                {
+                    case GamePadButtons.ThumbSticks_Left:
+                    case GamePadButtons.LeftStick:
+                        return _state.ThumbSticks.Left;
 
-                case GamePadButtons.ThumbSticks_Right:
-                case GamePadButtons.RightStick:
-                    return _state.ThumbSticks.Right;
-            }
+                    case GamePadButtons.ThumbSticks_Right:
+                    case GamePadButtons.RightStick:
+                        return _state.ThumbSticks.Right;
+                }
             return Vector2.Zero;
         }
 
@@ -214,6 +127,82 @@ namespace OpenVIII
         }
 
         private bool Release(GamePadButtons k, GamePadState _state) => !Press(k, _state);
+
+        #endregion Methods
+
+        #region Properties
+
+        protected static GamePadState Last_State => last_state;
+
+        protected static GamePadState State
+        {
+            get => state; set
+            {
+                last_state = state;
+                state = value;
+            }
+        }
+
+        #endregion Properties
+
+        protected FF8String ButtonString(Iap i)
+        {
+            short id = (short)i.id;
+            return new FF8String(new byte[] { (byte)FF8TextTagCode.Dialog, (byte)FF8TextTagDialog.CustomICON, (byte)(id & 0xFF), (byte)((id & 0xFF00) >> 8), i.palette });
+        }
+
+        protected override bool UpdateOnce()
+        {
+            State = Microsoft.Xna.Framework.Input.GamePad.GetState(PlayerIndex.One, GamePadDeadZone.Circular);
+            return false;
+        }
+
+        protected struct Iap
+        {
+            #region Fields
+
+            public Icons.ID id;
+            public byte palette;
+
+            #endregion Fields
+
+            #region Constructors
+
+            public Iap(Icons.ID id = Icons.ID.None, byte palette = 2)
+            {
+                this.id = id;
+                this.palette = palette;
+            }
+
+            #endregion Constructors
+        }
+
+        public InputGamePad(bool skip = true) : base(skip)
+        {
+            if (iap == null)
+            {
+                iap = new Dictionary<GamePadButtons, Iap>
+                {
+                    { GamePadButtons.Up, new Iap{ id= Icons.ID.D_Pad_Up, palette = 2 } },
+                    { GamePadButtons.Down, new Iap{ id= Icons.ID.D_Pad_Down, palette = 2 } },
+                    { GamePadButtons.Left, new Iap{ id= Icons.ID.D_Pad_Left, palette = 2 } },
+                    { GamePadButtons.Right, new Iap{ id= Icons.ID.D_Pad_Right, palette = 2 } },
+                    { GamePadButtons.X, new Iap {id = Icons.ID.Size_16x16_PSX_Square, palette = 4} },
+                    { GamePadButtons.Y, new Iap {id = Icons.ID.Size_16x16_PSX_Triangle, palette = 4} },
+                    { GamePadButtons.A, new Iap {id = Icons.ID.Size_16x16_PSX_Cross, palette = 4} },
+                    { GamePadButtons.B, new Iap {id = Icons.ID.Size_16x16_PSX_Circle, palette = 4} },
+                    { GamePadButtons.Back, new Iap {id = Icons.ID.SELECT, palette = 2} },
+                    { GamePadButtons.Start, new Iap {id = Icons.ID.START, palette = 2} },
+                    { GamePadButtons.Left_Shoulder, new Iap {id = Icons.ID.Size_16x08_PSX_L1, palette = 2} },
+                    { GamePadButtons.Right_Shoulder, new Iap {id = Icons.ID.Size_16x08_PSX_R1, palette = 2} },
+                    { GamePadButtons.Left_Trigger, new Iap {id = Icons.ID.Size_16x08_PSX_L2, palette = 2} },
+                    { GamePadButtons.Right_Trigger, new Iap {id = Icons.ID.Size_16x08_PSX_R2, palette = 2} },
+                };
+            }
+        }
+
+        public static Vector2 Distance(GamePadButtons stick, float speed) => Translate_Stick(stick, state) * (float)Distance(speed);
+
         public FF8String ButtonString(GamePadButtons gamePadButton, FF8TextTagKey key)
         {
             if (iap.ContainsKey(gamePadButton))
@@ -222,12 +211,31 @@ namespace OpenVIII
             }
             return "";
         }
-        protected FF8String ButtonString(Iap i)
-        {
-            short id = (short)i.id;
-            return new FF8String(new byte[] { (byte)FF8TextTagCode.Dialog, (byte)FF8TextTagDialog.CustomICON, (byte)(id & 0xFF), (byte)((id & 0xFF00) >> 8), i.palette });
-        }
 
-        #endregion Methods
+        public override bool ButtonTriggered(InputButton test, ButtonTrigger trigger = ButtonTrigger.None)
+        {
+            if (test != null && test.GamePadButton != GamePadButtons.None)
+            {
+                bool combotest = false;
+                if (test.Combo != null)
+                {
+                    foreach (InputButton item in test.Combo)
+                    {
+                        item.Trigger = ButtonTrigger.Press;
+                        if (!base.ButtonTriggered(item))
+                        {
+                            return false;
+                        }
+                    }
+                    combotest = true;
+                }
+                ButtonTrigger triggertest = test.Trigger | trigger;
+                return ((test.Combo == null || combotest) &&
+                    ((triggertest & ButtonTrigger.OnPress) != 0 && OnPress(test.GamePadButton)) ||
+                    ((triggertest & ButtonTrigger.OnRelease) != 0 && OnRelease(test.GamePadButton)) ||
+                    ((triggertest & ButtonTrigger.Press) != 0 && Press(test.GamePadButton, state)));
+            }
+            return false;
+        }
     }
 }
