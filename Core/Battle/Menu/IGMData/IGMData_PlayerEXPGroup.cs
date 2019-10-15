21b9de5bbfd8236aff5c41277d7cace56cf8e5f9
Robert Russell <peeceseven@gmail.com>
2019-10-15 12:33:41 -0400

    Update IGMData_PlayerEXPGroup.cs
    Disposible for Box varible.

--- Core/Battle/Menu/IGMData/IGMData_PlayerEXPGroup.cs
+++ Core/Battle/Menu/IGMData/IGMData_PlayerEXPGroup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using OpenVIII.IGMDataItem;
+using System;
 using System.Collections.Concurrent;
 
 namespace OpenVIII
@@ -12,7 +13,7 @@ namespace OpenVIII
         {
             #region Classes
 
-            private class IGMData_PlayerEXPGroup : IGMData.Group.Base
+            private class IGMData_PlayerEXPGroup : IGMData.Group.Base, IDisposable
             {
                 #region Fields
 
@@ -177,6 +178,41 @@ namespace OpenVIII
                     }
                     header.Width = Width;
                 }
+
+                #region IDisposable Support
+                private bool disposedValue = false; // To detect redundant calls
+
+                protected virtual void Dispose(bool disposing)
+                {
+                    if (!disposedValue)
+                    {
+                        if (disposing)
+                        {
+                            // TODO: dispose managed state (managed objects).
+                        }
+
+                        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                        // TODO: set large fields to null.
+                        header.Dispose();
+                        disposedValue = true;
+                    }
+                }
+
+                // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+                 ~IGMData_PlayerEXPGroup() {
+                   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+                   Dispose(false);
+                 }
+
+                // This code added to correctly implement the disposable pattern.
+                public void Dispose()
+                {
+                    // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+                    Dispose(true);
+                    // TODO: uncomment the following line if the finalizer is overridden above.
+                    GC.SuppressFinalize(this);
+                }
+                #endregion
 
                 #endregion Methods
             }
