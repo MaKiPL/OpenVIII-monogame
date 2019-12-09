d93ac2394e2e4f3c4e0cdd812c40953ba0eda7a9
Robert Russell <peeceseven@gmail.com>
2019-12-09 01:34:08 -0500

    removed page calc from enemy attacks.
    removed the page sount. will probably auto detect pages for each enemy. will normally be only 1.
    
    also core was updated with enemy attacks path.

--- Core/Battle/Menu/IGMData/Pool/Enemy_Attacks.cs
+++ Core/Battle/Menu/IGMData/Pool/Enemy_Attacks.cs
@@ -6,7 +6,7 @@ namespace OpenVIII.IGMData.Pool
     {
         public static Enemy_Attacks Create(Rectangle pos, Damageable damageable = null, bool battle = false, int count = 4)
         {
-            Enemy_Attacks r = Create<Enemy_Attacks>(count + 1, 3, new IGMDataItem.Box { Pos = pos, Title = Icons.ID.ITEM }, count, 198 / count + 1, damageable, battle: battle);
+            Enemy_Attacks r = Create<Enemy_Attacks>(count + 1, 3, new IGMDataItem.Box { Pos = pos, Title = Icons.ID.ITEM }, count, 1, damageable, battle: battle);
             if (battle)
                 r.Target_Group = Target.Group.Create(r.Damageable);
             return r;
