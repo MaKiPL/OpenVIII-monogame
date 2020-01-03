ab6d233fe91da854c7b90ca3a1a59ab99e3a35aa
Robert Russell <peeceseven@gmail.com>
2020-01-02 22:36:41 -0500

    added quad conversion code to tiles

--- Core/Field/Tile.cs
+++ Core/Field/Tile.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace OpenVIII
 {
@@ -110,6 +112,73 @@ namespace OpenVIII
             }
             public Rectangle GetRectangle() => new Rectangle(X, Y, size, size);
             public static implicit operator Vector3(Tile @in) => new Vector3(@in.X, @in.Y, @in.Zfloat);
+            /// <summary>
+            /// TopLeft Cord
+            /// </summary>
+            /// <param name="in"></param>
+            public static implicit operator Vector2(Tile @in) => new Vector2(@in.SourceX,@in.SourceY);
+            /// <summary>
+            /// Size of Texture Segment
+            /// </summary>
+            public static readonly Vector2 TextureSize = new Vector2(128, 256);
+            /// <summary>
+            /// TopLeft UV
+            /// </summary>
+            public Vector2 UV => (Vector2)this / TextureSize;
+            List<VertexPositionTexture> GetQuad()
+            {
+                var vpts = GetCorners(); // 4 unique corners.
+                //create 2 triangles
+                List<VertexPositionTexture> r = new List<VertexPositionTexture>
+                {
+                    vpts[0],
+                    vpts[1],
+                    vpts[3],
+
+                    vpts[1],
+                    vpts[2],
+                    vpts[3],
+                };
+                return r;
+
+            }
+            List<VertexPositionTexture> GetCorners()
+            {
+
+                Vector2 sizeVertex = new Vector2(size);
+                Vector2 sizeUV = new Vector2(size)/TextureSize;
+                List<VertexPositionTexture> r = new List<VertexPositionTexture>{
+                    new VertexPositionTexture(this,UV),
+                    new VertexPositionTexture(this,UV),
+                    new VertexPositionTexture(this,UV),
+                    new VertexPositionTexture(this,UV),
+                };
+                for (int i = 1; i < r.Count; i++)
+                {
+                    var vpt = r[0];
+                    switch(i)
+                    {
+                        case 0://top left
+                            break;
+                        case 1://top right
+                            vpt.Position.X += sizeVertex.X;
+                            vpt.TextureCoordinate.X += sizeUV.X;
+                            break;
+                        case 2://bottom right
+                            vpt.Position.X += sizeVertex.X;
+                            vpt.Position.Y += sizeVertex.Y;
+                            vpt.TextureCoordinate.X += sizeUV.X;
+                            vpt.TextureCoordinate.Y += sizeUV.Y;
+                            break;
+                        case 3://bottom left
+                            vpt.Position.Y += sizeVertex.Y;
+                            vpt.TextureCoordinate.Y += sizeUV.Y;
+                            break;
+                    }
+                }
+                return r;
+            }
+
         }
     }
 }
