1c78c05f119f908807faf6d79039a45268a1602f
Robert Russell <peeceseven@gmail.com>
2020-01-05 22:38:38 -0500

    extracted some types from feilds to their own files.

---
+++ Core/Field/BlendMode.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenVIII
+{
+    public static partial class Module_field_debug
+    {
+        #region Enums
+
+        public enum BlendMode : byte
+        {
+            halfadd,
+            add,
+            subtract,
+            quarteradd,
+            none,
+        }
+
+        #endregion Enums
+    }
+}
---
+++ Core/Field/TextureType.cs
@@ -0,0 +1,36 @@
+﻿namespace OpenVIII
+{
+    public static partial class Module_field_debug
+    {
+        //private static int palettes = 24; // or 16;
+
+        #region Classes
+
+        private class TextureType
+        {
+            #region Fields
+
+            public byte Palettes;
+            public byte SkippedPalettes = 8;
+            public byte TexturePages;
+            public byte Type = 1;
+            private const ushort THeight = 256;
+            private const ushort TWidth = 128;
+
+            #endregion Fields
+
+            #region Properties
+
+            public int BytesSkippedPalettes => bytesPerPalette * SkippedPalettes;
+            public uint FileSize => checked((uint)(PaletteSectionSize + (Width * THeight)));
+            public uint PaletteSectionSize => checked((uint)(bytesPerPalette * Palettes));
+            public ushort Width => checked((ushort)(TWidth * TexturePages));
+
+            #endregion Properties
+
+            // first 8 are junk and are not used.
+        }
+
+        #endregion Classes
+    }
+}
