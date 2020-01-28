8eaf8e85c8155a990b3cd58c3626bd1f934e5251
Robert Russell <peeceseven@gmail.com>
2020-01-28 10:45:32 -0500

    Added a basic animation for lava stage 31 I think.

---
+++ Core/Battle/Stage/Animation.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpenVIII.Battle
+{
+    public partial class Stage
+    {
+        #region Classes
+
+        private class Animation
+        {
+            #region Fields
+
+            public int FrameNumber = 0;
+            public TimeSpan frametime = TimeSpan.FromMilliseconds(1000 / 10);
+            public TimeSpan time = TimeSpan.Zero;
+            private byte Clut = 4;
+            private byte Cols = 4;
+            private int Height = 64;
+            private List<Quad> qs;
+            private Rectangle Rectangle;
+            private byte Rows = 2;
+            private Vector2 start;
+            private byte TexturePage = 4;
+            private List<Triangle> ts;
+            private int Width = 64;
+
+            #endregion Fields
+
+            #region Constructors
+
+            public Animation(int width, int height, byte clut, byte texturePage, byte cols, byte rows, int texWidth, ModelGroups _mg)
+            {
+                Width = width;
+                Height = height;
+                Clut = clut;
+                TexturePage = texturePage;
+                Cols = cols;
+                Rows = rows;
+                start = CalculateUV(Vector2.Zero, texturePage, texWidth);
+                Rectangle = new Rectangle(0, 0, width, height);
+
+                qs = (from modelgroups in _mg
+                      from mg in modelgroups
+                      from q in mg.quads
+                      select q).Where(q => Rectangle.Contains(q.Rectangle) && q.TexturePage == texturePage).ToList();
+                ts = (from modelgroups in _mg
+                      from mg in modelgroups
+                      from q in mg.triangles
+                      select q).Where(q => Rectangle.Contains(q.Rectangle) && q.TexturePage == texturePage).ToList();
+            }
+
+            #endregion Constructors
+
+            #region Properties
+
+            public int Frames => Rows * Cols;
+
+            #endregion Properties
+
+            #region Methods
+
+            public void Update()
+            {
+                time += Memory.gameTime.ElapsedGameTime;
+                if (time >= frametime)
+                {
+                    time = TimeSpan.Zero;
+                    int Last = FrameNumber;
+                    FrameNumber += 1;
+                    if (FrameNumber >= Frames)
+                        FrameNumber = 0;
+
+                    int lastcol = Last % Cols;
+                    int lastrow = Last / Cols;
+                    int col = FrameNumber % Cols;
+                    int row = FrameNumber / Cols;
+
+                    foreach (Quad q in qs)
+                    {
+                        for (int i = 0; i < q.UVs.Count; i++)
+                        {
+                            Vector2 uv = q.UVs[i];
+                            uv.X += Width * (col - lastcol);
+                            uv.Y += Height * (row - lastrow);
+                            q.UVs[i] = uv;
+                        }
+                    }
+                    foreach (Triangle q in ts)
+                    {
+                        for (int i = 0; i < q.UVs.Count; i++)
+                        {
+                            Vector2 uv = q.UVs[i];
+                            uv.X += Width * (col - lastcol);
+                            uv.Y += Height * (row - lastrow);
+                            q.UVs[i] = uv;
+                        }
+                    }
+                }
+            }
+
+            #endregion Methods
+        }
+
+        #endregion Classes
+    }
+}
--- Core/Battle/Stage/Stage.cs
+++ Core/Battle/Stage/Stage.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace OpenVIII.Battle
 {
@@ -16,12 +17,14 @@ namespace OpenVIII.Battle
         /// </summary>
         private static readonly float[] skyRotators = { 0x4, 0x4, 0x4, 0x4, 0x0, 0x0, 0x4, 0x4, 0x0, 0x0, 0x4, 0x4, 0x0, 0x0, 0x0, 0x0, 0x0, 0x4, 0x4, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x4, 0x0, 0x4, 0x4, 0x0, 0x10, 0x10, 0x0, 0x0, 0x0, 0x0, 0x0, 0x8, 0x2, 0x0, 0x0, 0x8, 0xfffc, 0xfffc, 0x0, 0x0, 0x0, 0x4, 0x0, 0x8, 0x0, 0x4, 0x4, 0x0, 0x4, 0x0, 0x4, 0xfffc, 0x8, 0xfffc, 0xfffc, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x4, 0x0, 0x4, 0x4, 0x0, 0x0, 0x4, 0x4, 0x0, 0x0, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x8, 0x0, 0x8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x4, 0x4, 0x4, 0x4, 0x0, 0x0, 0x4, 0x4, 0x8, 0xfffc, 0x4, 0x4, 0x4, 0x4, 0x8, 0x8, 0x4, 0xfffc, 0xfffc, 0x8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0xfffc, 0x0, 0x0, 0x0, 0x0, 0x8, 0x8, 0x0, 0x8, 0xfffc, 0x0, 0x0, 0x8, 0x0, 0x0, 0x0, 0x0, 0x0, 0x4, 0x4, 0x4, 0x4, 0x4, 0x0, 0x0, 0x8, 0x0, 0x8, 0x8 };
 
+        private List<Animation> Animations;
+
         /// <summary>
         /// a rotator is a float that holds current axis rotation for sky. May be malformed by skyRotators or TimeCompression magic
         /// </summary>
         private float localRotator = 0.0f;
 
-        private ModelGroup[] modelGroups;
+        private ModelGroups modelGroups;
 
         #endregion Fields
 
@@ -34,6 +37,7 @@ namespace OpenVIII.Battle
 
         #region Properties
 
+        public static byte Scenario { get; private set; }
         public int Height { get; private set; }
 
         public TextureHandler[] textures { get; private set; }
@@ -46,11 +50,18 @@ namespace OpenVIII.Battle
 
         private Matrix worldMatrix => Module_battle_debug.WorldMatrix;
 
-        public static byte Scenario { get; private set; }
-
         #endregion Properties
 
         #region Methods
+
+        public static Vector2 CalculateUV(Vector2 UV, byte texPage, int texWidth)
+        {
+            //old code from my wiki page
+            //Float U = (float)U_Byte / (float)(TIM_Texture_Width * 2) + ((float)Texture_Page / (TIM_Texture_Width * 2));
+            float fU = (UV.X + texPage * 128f) / texWidth;
+            float fV = UV.Y / 256.0f;
+            return new Vector2(fU, fV);
+        }
 
         public static BinaryReader Open()
         {
@@ -87,14 +98,15 @@ namespace OpenVIII.Battle
                     ObjectsGroup.Read(MainSection.Group4Pointer,br)
             };
 
-            s.modelGroups = new ModelGroup[4]
-            {
-                    ModelGroup.Read(objectsGroups[0].objectListPointer,br),
-                    ModelGroup.Read(objectsGroups[1].objectListPointer,br),
-                    ModelGroup.Read(objectsGroups[2].objectListPointer,br),
-                    ModelGroup.Read(objectsGroups[3].objectListPointer,br)
-            };
+            s.modelGroups = new ModelGroups(
+                    ModelGroup.Read(objectsGroups[0].objectListPointer, br),
+                    ModelGroup.Read(objectsGroups[1].objectListPointer, br),
+                    ModelGroup.Read(objectsGroups[2].objectListPointer, br),
+                    ModelGroup.Read(objectsGroups[3].objectListPointer, br)
+            );
+
             s.ReadTexture(MainSection.TexturePointer, br);
+
             return s;
         }
 
@@ -116,7 +128,7 @@ namespace OpenVIII.Battle
                 TextureEnabled = true
             })
             {
-                for (int n = 0; n < (modelGroups?.Length ?? 0); n++)
+                for (int n = 0; n < (modelGroups?.Count ?? 0); n++)
                     foreach (Model b in modelGroups[n])
                     {
                         GeometryVertexPosition vpt = GetVertexBuffer(b);
@@ -149,15 +161,15 @@ namespace OpenVIII.Battle
         }
 
         public void Update()
-        { }
-
-        private static Vector2 CalculateUV(byte U, byte V, byte texPage, int texWidth)
         {
-            //old code from my wiki page
-            //Float U = (float)U_Byte / (float)(TIM_Texture_Width * 2) + ((float)Texture_Page / (TIM_Texture_Width * 2));
-            float fU = (float)U / texWidth + (((float)texPage * 128) / texWidth);
-            float fV = V / 256.0f;
-            return new Vector2(fU, fV);
+            if (Scenario == 31)
+            {
+                if (Animations == null)
+                {
+                    Animations = new List<Animation> { new Animation(64, 64, 4, 4, 4, 2, Width, modelGroups) };
+                }
+                Animations.ForEach(x => x.Update());
+            }
         }
 
         private static byte GetClutId(ushort clut)
@@ -196,12 +208,12 @@ namespace OpenVIII.Battle
                 Vertex A = model.vertices[model.triangles[i].A];
                 Vertex B = model.vertices[model.triangles[i].B];
                 Vertex C = model.vertices[model.triangles[i].C];
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)A.X / 100, (float)A.Y / 100, (float)A.Z / 100),
-                    CalculateUV(model.triangles[i].U2, model.triangles[i].V2, model.triangles[i].TexturePage, Width)));
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)B.X / 100, (float)B.Y / 100, (float)B.Z / 100),
-                    CalculateUV(model.triangles[i].U3, model.triangles[i].V3, model.triangles[i].TexturePage, Width)));
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)C.X / 100, (float)C.Y / 100, (float)C.Z / 100),
-                    CalculateUV(model.triangles[i].U1, model.triangles[i].V1, model.triangles[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(A,
+                    CalculateUV(model.triangles[i].UVs[1], model.triangles[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(B,
+                    CalculateUV(model.triangles[i].UVs[2], model.triangles[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(C,
+                    CalculateUV(model.triangles[i].UVs[0], model.triangles[i].TexturePage, Width)));
                 bs_renderer_supplier.Add(new GeometryInfoSupplier()
                 {
                     bQuad = false,
@@ -219,20 +231,20 @@ namespace OpenVIII.Battle
 
                 //triangluation wing-reorder
                 //1 2 4
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)A.X / 100, (float)A.Y / 100, (float)A.Z / 100),
-                    CalculateUV(model.quads[i].U1, model.quads[i].V1, model.quads[i].TexturePage, Width)));
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)B.X / 100, (float)B.Y / 100, (float)B.Z / 100),
-                    CalculateUV(model.quads[i].U2, model.quads[i].V2, model.quads[i].TexturePage, Width)));
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)D.X / 100, (float)D.Y / 100, (float)D.Z / 100),
-                    CalculateUV(model.quads[i].U4, model.quads[i].V4, model.quads[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(A,
+                    CalculateUV(model.quads[i].UVs[0], model.quads[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(B,
+                    CalculateUV(model.quads[i].UVs[1], model.quads[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(D,
+                    CalculateUV(model.quads[i].UVs[3], model.quads[i].TexturePage, Width)));
 
                 //1 3 4
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)D.X / 100, (float)D.Y / 100, (float)D.Z / 100),
-                    CalculateUV(model.quads[i].U4, model.quads[i].V4, model.quads[i].TexturePage, Width)));
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)C.X / 100, (float)C.Y / 100, (float)C.Z / 100),
-                    CalculateUV(model.quads[i].U3, model.quads[i].V3, model.quads[i].TexturePage, Width)));
-                vptDynamic.Add(new VertexPositionTexture(new Vector3((float)A.X / 100, (float)A.Y / 100, (float)A.Z / 100),
-                    CalculateUV(model.quads[i].U1, model.quads[i].V1, model.quads[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(D,
+                    CalculateUV(model.quads[i].UVs[3], model.quads[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(C,
+                    CalculateUV(model.quads[i].UVs[2], model.quads[i].TexturePage, Width)));
+                vptDynamic.Add(new VertexPositionTexture(A,
+                    CalculateUV(model.quads[i].UVs[0], model.quads[i].TexturePage, Width)));
 
                 bs_renderer_supplier.Add(new GeometryInfoSupplier()
                 {
@@ -251,9 +263,57 @@ namespace OpenVIII.Battle
         private void ReadTexture(uint texturePointer, BinaryReader br)
         {
             TIM2 textureInterface = new TIM2(br, texturePointer);
+            if (Memory.EnableDumpingData || EnableDumpingData)
+            {
+                IEnumerable<Model> temp = (from mg in modelGroups
+                                           from m in mg
+                                           select m);
+                //IOrderedEnumerable<byte> cluts = temp.SelectMany(x => x.quads.Select(y => y.clut)).Union(temp.SelectMany(x => x.triangles.Select(y => y.clut))).Distinct().OrderBy(x => x);
+                //IOrderedEnumerable<byte> unks = temp.SelectMany(x => x.quads.Select(y => y.UNK)).Union(temp.SelectMany(x => x.triangles.Select(y => y.UNK))).Distinct().OrderBy(x => x);
+                //IOrderedEnumerable<byte> hides = temp.SelectMany(x => x.quads.Select(y => y.bHide)).Union(temp.SelectMany(x => x.triangles.Select(y => y.bHide))).Distinct().OrderBy(x => x);
+                //IOrderedEnumerable<byte> gpu = temp.SelectMany(x => x.quads.Select(y => y.GPU)).Union(temp.SelectMany(x => x.triangles.Select(y => y.GPU))).Distinct().OrderBy(x => x);
+                //IOrderedEnumerable<Color> color = temp.SelectMany(x => x.quads.Select(y => y.Color)).Union(temp.SelectMany(x => x.triangles.Select(y => y.Color))).Distinct().OrderBy(x => x.R).ThenBy(x => x.G).ThenBy(x => x.B);
+                var tuv = (from m in temp
+                           from t in m.triangles
+                           select new { t.clut, t.TexturePage, t.MinUV, t.MaxUV, t.Rectangle }).Distinct().OrderBy(x => x.TexturePage).ThenBy(x => x.clut)/*.Where(x => x.Rectangle.Height > 0 && x.Rectangle.Width > 0)*/.ToList();
+                var quv = (from m in temp
+                           from q in m.quads
+                           select new { q.clut, q.TexturePage, q.MinUV, q.MaxUV, q.Rectangle }).Distinct().OrderBy(x => x.TexturePage).ThenBy(x => x.clut)/*.Where(x => x.Rectangle.Height > 0 && x.Rectangle.Width > 0)*/.ToList();
+                var all = tuv.Union(quv);
+                foreach (var tpGroup in all.GroupBy(x => x.TexturePage))
+                {
+                    byte texturepage = tpGroup.Key;
+                    foreach (var clutGroup in tpGroup.GroupBy(x => x.clut))
+                    {
+                        byte clut = clutGroup.Key;
+                        string filename = Path.GetFileNameWithoutExtension(Memory.Encounters.Filename);
+                        string p = Path.Combine(Path.GetTempPath(), "Battle Stages", filename, "Reference");
+                        Directory.CreateDirectory(p);
+                        filename = $"{filename}_{clut}_{texturepage}.png";
+                        using (Texture2D tex = textureInterface.GetTexture(clut))
+                        using (RenderTarget2D tmp = new RenderTarget2D(Memory.graphics.GraphicsDevice, 256, 256))
+                        {
+                            Memory.graphics.GraphicsDevice.SetRenderTarget(tmp);
+                            Memory.SpriteBatchStartAlpha();
+                            Memory.graphics.GraphicsDevice.Clear(Color.TransparentBlack);
+                            foreach (Rectangle r in clutGroup.Select(x => x.Rectangle))
+                            {
+                                Rectangle src = r;
+                                Rectangle dst = r;
+                                src.Offset(texturepage * 128, 0);
+                                Memory.spriteBatch.Draw(tex, dst, src, Color.White);
+                            }
+                            Memory.SpriteBatchEnd();
+                            Memory.graphics.GraphicsDevice.SetRenderTarget(null);
+                            using (FileStream fs = new FileStream(Path.Combine(p, filename), FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                                tmp.SaveAsPng(fs, 256, 256);
+                        }
+                    }
+                }
+            }
             Width = textureInterface.GetWidth;
             Height = textureInterface.GetHeight;
-            string path = Path.Combine(Path.GetTempPath(), "Battle Stages");
+            string path = Path.Combine(Path.GetTempPath(), "Battle Stages", Path.GetFileNameWithoutExtension(Memory.Encounters.Filename));
             Directory.CreateDirectory(path);
 
             if (Memory.EnableDumpingData || EnableDumpingData)
@@ -262,6 +322,7 @@ namespace OpenVIII.Battle
                 if (!File.Exists(fullpath))
                     textureInterface.SaveCLUT(fullpath);
             }
+
             textures = new TextureHandler[textureInterface.GetClutCount];
             for (ushort i = 0; i < textureInterface.GetClutCount; i++)
             {
